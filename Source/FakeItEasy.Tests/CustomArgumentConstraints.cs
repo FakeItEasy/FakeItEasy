93e47dbf19e9839d49e488c62482a3860ebd4534
patrik.hagne <devnull@localhost>
2010-02-28 15:09:17 +0100

    Renamed test file to include the word constraint.

---
+++ Source/FakeItEasy.Tests/CustomArgumentConstraints.cs
@@ -0,0 +1,34 @@
+namespace FakeItEasy.Tests
+{
+    using System.Collections;
+    using System.Globalization;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using FakeItEasy.Api;
+    using FakeItEasy.Expressions;
+
+    public static class CustomArgumentConstraints
+    {
+        public static ArgumentConstraint<T> IsThisSequence<T>(this ArgumentConstraintScope<T> scope, T collection) where T : IEnumerable
+        {
+            return ArgumentConstraint.Create(scope, x => x.Cast<object>().SequenceEqual(collection.Cast<object>()), "Same sequence");
+        }
+
+        public static ArgumentConstraint<T> IsThisSequence<T>(this ArgumentConstraintScope<T> scope, params object[] collection) where T : IEnumerable
+        {
+            return ArgumentConstraint.Create(scope, x => x.Cast<object>().SequenceEqual(collection.Cast<object>()), "Same sequence");
+        }
+
+        
+        public static ArgumentConstraint<Expression> ProducesValue(this ArgumentConstraintScope<Expression> scope, object expectedValue)
+        {
+            return ArgumentConstraint.Create(scope, x => object.Equals(expectedValue, ExpressionManager.GetValueProducedByExpression(x)), 
+			                                string.Format(CultureInfo.InvariantCulture, "Expression that produces the value {0}", expectedValue));
+        }
+
+        public static ArgumentConstraint<FakeObject> Fakes(this ArgumentConstraintScope<FakeObject> scope, object fakedObject)
+        {
+            return ArgumentConstraint.Create(scope, x => x.Equals(Fake.GetFakeObject(fakedObject)), "Specified FakeObject");
+        }
+    }
+}
