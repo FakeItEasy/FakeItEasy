f07145462f9d8cdbe40cc8a9448d268648201809
patrik@hagne.se <patrik@hagne.se>
2010-03-06 12:35:29 +0100

    Moved using directives.

--- Source/FakeItEasy/VisualBasic/IRecordingCallRuleFactory.cs
+++ Source/FakeItEasy/VisualBasic/IRecordingCallRuleFactory.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using FakeItEasy.Configuration;
-using FakeItEasy.Api;
-using System.Reflection;
-using FakeItEasy.Assertion;
-
 namespace FakeItEasy.VisualBasic
 {
+    using FakeItEasy.Api;
+
     /// <summary>
     /// A factory that creates instances of the RecordingCallRuleType.
     /// </summary>
