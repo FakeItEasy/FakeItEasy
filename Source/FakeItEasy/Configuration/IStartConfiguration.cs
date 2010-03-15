31795b388fe2fec26597023f90e9e3eb871f712b
patrik@hagne.se <patrik@hagne.se>
2010-03-15 19:34:19 +0100

    Moved IAnyCallConfiguration to a file of its own.

--- Source/FakeItEasy/Configuration/IStartConfiguration.cs
+++ Source/FakeItEasy/Configuration/IStartConfiguration.cs
@@ -35,9 +35,17 @@
         IAnyCallConfiguration AnyCall();
     }
 
+    /// <summary>
+    /// Configuration for any call to a faked object.
+    /// </summary>
     public interface IAnyCallConfiguration
         : IVoidConfiguration
     {
+        /// <summary>
+        /// Matches calls that has the return type specified in the generic type parameter.
+        /// </summary>
+        /// <typeparam name="TMember">The return type of the members to configure.</typeparam>
+        /// <returns>A configuration object.</returns>
         IReturnValueArgumentValidationConfiguration<TMember> WithReturnType<TMember>();
     }
 }
