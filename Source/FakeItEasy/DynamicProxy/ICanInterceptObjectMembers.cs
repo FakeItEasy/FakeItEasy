99a670a18f52c6def39668fbbcd5999cc9848ed4
Patrik Hägne <patrik@hagne.se>
2010-04-26 19:55:46 +0200

    Made the ICanInterceptObjectMembers-interface public to make DynamicProxy2 work with the signed assemblies.

--- Source/FakeItEasy/DynamicProxy/ICanInterceptObjectMembers.cs
+++ Source/FakeItEasy/DynamicProxy/ICanInterceptObjectMembers.cs
@@ -3,8 +3,10 @@ namespace FakeItEasy.DynamicProxy
     /// <summary>
     /// An interface implemented by DynamicProxy proxies in order to let them
     /// intercept object members.
+    /// 
+    /// NOT INTENDED FOR USE! Note that this interface WILL be removed from future versions of FakeItEasy.
     /// </summary>
-    internal interface ICanInterceptObjectMembers
+    public interface ICanInterceptObjectMembers
     {
         string ToString();
 
