f562b6cec005b5ca2b95ba8be0a032b45ecd33de
patrik.hagne <devnull@localhost>
2010-07-27 22:49:13 +0200

    Implemented start och OrderedFakeAsserter.

--- Source/FakeItEasy.Tests/Core/OrderedFakeAsserterTests.cs
+++ Source/FakeItEasy.Tests/Core/OrderedFakeAsserterTests.cs
@@ -1,22 +1,101 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using FakeItEasy.Configuration;
-using FakeItEasy.Core;
-using NUnit.Framework.Constraints;
-using System.Linq.Expressions;
-using System.Collections.ObjectModel;
-using System.Reflection;
-using FakeItEasy.Expressions;
-using System.IO;
-
 namespace FakeItEasy.Tests.Core
 {
+    using System;
+    using FakeItEasy.Core;
+    using NUnit.Framework;
+    using System.Linq;
+    using System.Collections.Generic;
+
     [TestFixture]
     public class OrderedFakeAsserterTests
     {
+        private IFakeAsserter innerAsserter;
 
+        [SetUp]
+        public void SetUp()
+        {
+            this.innerAsserter = A.Fake<IFakeAsserter>();
+        }
+
+        private OrderedFakeAsserter CreateAsserter(IEnumerable<IFakeObjectCall> calls)
+        {
+            return new OrderedFakeAsserter(calls, this.innerAsserter);
+        }
+
+        [Test]
+        public void Should_call_fake_asserter()
+        {
+            // Arrange
+            var orderedAsserter = this.CreateAsserter(Enumerable.Empty<IFakeObjectCall>());
+
+            Func<IFakeObjectCall, bool> callPredicate = x => true;
+            Func<int, bool> repeatPredicate = x => true;
+
+            // Act
+            orderedAsserter.AssertWasCalled(callPredicate, "call description", repeatPredicate, "repeat description");
+            
+            // Assert
+            A.CallTo(() => 
+                this.innerAsserter.AssertWasCalled(callPredicate, "call description", repeatPredicate, "repeat description"))
+                .MustHaveHappened();
+        }
+
+        [Test]
+        public void Should_fail_when_calls_did_not_happen_in_order()
+        {
+            // Arrange
+            var calls = A.CollectionOfFake<IFakeObjectCall>(2);
+
+            var orderedAsserter = this.CreateAsserter(calls);
+
+            Func<IFakeObjectCall, bool> secondCallPredicate = x => object.ReferenceEquals(calls[1], x);
+            Func<IFakeObjectCall, bool> firstCallPredicate = x => object.ReferenceEquals(calls[0], x);
+
+            // Act
+            orderedAsserter.AssertWasCalled(secondCallPredicate, "foo", x => x == 1, "foo");
+            
+            // Assert
+            Assert.Throws<ExpectationException>(() =>
+                orderedAsserter.AssertWasCalled(firstCallPredicate, "foo", x => x == 1, "foo"));
+        }
+
+        [Test]
+        public void Should_not_fail_when_calls_happened_in_order()
+        {
+            // Arrange
+            var calls = A.CollectionOfFake<IFakeObjectCall>(2);
+
+            var orderedAsserter = this.CreateAsserter(calls);
+
+            Func<IFakeObjectCall, bool> secondCallPredicate = x => object.ReferenceEquals(calls[1], x);
+            Func<IFakeObjectCall, bool> firstCallPredicate = x => object.ReferenceEquals(calls[0], x);
+
+            // Act
+            orderedAsserter.AssertWasCalled(firstCallPredicate, "foo", x => x == 1, "foo");
+
+            // Assert
+            Assert.DoesNotThrow(() =>
+                orderedAsserter.AssertWasCalled(secondCallPredicate, "foo", x => x == 1, "foo"));
+        }
+
+        [Test]
+        public void Should_fail_when_calls_happened_in_order_but_repeat_is_not_correct()
+        {
+            // Arrange
+            var calls = A.CollectionOfFake<IFakeObjectCall>(2);
+            calls.Add(calls[0]);
+
+            var orderedAsserter = this.CreateAsserter(calls);
+
+            Func<IFakeObjectCall, bool> secondCallPredicate = x => object.ReferenceEquals(calls[1], x);
+            Func<IFakeObjectCall, bool> firstCallPredicate = x => object.ReferenceEquals(calls[0], x);
+
+            // Act
+            orderedAsserter.AssertWasCalled(firstCallPredicate, "foo", x => x == 2, "foo");
+
+            // Assert
+            Assert.Throws<ExpectationException>(() =>
+                orderedAsserter.AssertWasCalled(secondCallPredicate, "foo", x => x == 1, "foo"));
+        }
     }
 }
--- Source/FakeItEasy/Core/OrderedFakeAsserter.cs
+++ Source/FakeItEasy/Core/OrderedFakeAsserter.cs
@@ -2,14 +2,44 @@ namespace FakeItEasy.Core
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
 
     internal class OrderedFakeAsserter
         : IFakeAsserter
     {
-        void IFakeAsserter.AssertWasCalled(Func<IFakeObjectCall, bool> callPredicate, string callDescription, Func<int, bool> repeatPredicate, string repeatDescription)
+        private IFakeAsserter asserter;
+        private Queue<IFakeObjectCall> calls;
+
+        public OrderedFakeAsserter(IEnumerable<IFakeObjectCall> calls, IFakeAsserter asserter)
         {
-            throw new NotImplementedException();
+            this.asserter = asserter;
+            this.calls = new Queue<IFakeObjectCall>(calls);
+        }
+
+        public void AssertWasCalled(Func<IFakeObjectCall, bool> callPredicate, string callDescription, Func<int, bool> repeatPredicate, string repeatDescription)
+        {
+            this.asserter.AssertWasCalled(callPredicate, callDescription, repeatPredicate, repeatDescription);
+
+            this.RemoveCallsToSatisfyRepeatPredicate(callPredicate, repeatPredicate);
+        }
+
+        private void RemoveCallsToSatisfyRepeatPredicate(Func<IFakeObjectCall, bool> callPredicate, Func<int, bool> repeatPredicate)
+        {
+            int numberOfCallsFound = 0;
+
+            while (!repeatPredicate(numberOfCallsFound))
+            {
+                if (this.calls.Count == 0)
+                {
+                    throw new ExpectationException();
+                }
+
+                var currentCall = this.calls.Dequeue();
+
+                if (callPredicate(currentCall))
+                {
+                    numberOfCallsFound++;
+                }
+            }
         }
     }
 }
