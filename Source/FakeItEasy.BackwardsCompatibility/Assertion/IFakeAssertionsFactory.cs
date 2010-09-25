2548a3d5a943b864a2327bf47c6a486dfbe0e8f0
patrik.hagne <devnull@localhost>
2010-09-25 18:35:14 +0200

    Removed old projects.

--- Source/FakeItEasy.BackwardsCompatibility/Assertion/IFakeAssertionsFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace FakeItEasy.Assertion
-{
-    using FakeItEasy.Core;
-
-    internal interface IFakeAssertionsFactory
-    {
-        IFakeAssertions<TFake> CreateAsserter<TFake>(FakeManager fake);
-    }
-}
