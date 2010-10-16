19d51da9b0d1d5188c9405d18062a0d741239da3
patrik.hagne <devnull@localhost>
2010-10-16 20:28:30 +0200

    Added dependency handling via NuPack.

---
+++ Source/FakeItEasy.Examples/NUnitSampleTests.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+
+namespace Legend.Fakes.Examples {
+    [TestFixture]
+    public class NUnitSampleTests {
+        [Test]
+        public void SomePassingTest() {
+            Assert.AreEqual(5, 5);
+        }
+
+        [Test]
+        public void SomeFailingTest() {
+            Assert.Greater(5, 7);
+        }
+    }
+}
