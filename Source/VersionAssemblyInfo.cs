336329c9fdd726d0718494b89663bff64d379bbc
patrik@hagne.se <patrik@hagne.se>
2011-02-27 15:37:36 +0100

    Updated build script to merge the Silverlight version correctly.

--- Source/VersionAssemblyInfo.cs
+++ Source/VersionAssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("1.6.4071.215")]
-[assembly: AssemblyFileVersion("1.6.4071.215")]
-[assembly: AssemblyConfiguration("Release built on 2011-02-23 20:13:09Z")]
+[assembly: AssemblyVersion("1.6.4075.221")]
+[assembly: AssemblyFileVersion("1.6.4075.221")]
+[assembly: AssemblyConfiguration("Release built on 2011-02-27 15:36:03Z")]
 
 
