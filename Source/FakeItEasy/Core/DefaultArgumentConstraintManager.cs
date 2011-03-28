e3cf11f40b25a8b5933ea0d892d0119f778d8eef
patrik@hagne.se <patrik@hagne.se>
2011-03-28 23:23:00 +0200

    Implemented the new argument constraint manager.

---
+++ Source/FakeItEasy/Core/DefaultArgumentConstraintManager.cs
@@ -0,0 +1,78 @@
+﻿namespace FakeItEasy.Core
+{
+    using System;
+
+    internal class DefaultArgumentConstraintManager<T>
+        : IArgumentConstraintManager<T>
+    {
+        private readonly Action<IArgumentConstraint2> onConstraintCreated;
+
+        public DefaultArgumentConstraintManager(Action<IArgumentConstraint2> onConstraintCreated)
+        {
+            this.onConstraintCreated = onConstraintCreated;
+        }
+
+        public IArgumentConstraintManager<T> Not
+        {
+            get { return new NotArgumentConstraintManager(this); }
+        }
+
+        public T Matches(Func<T, bool> predicate, Action<IOutputWriter> descriptionWriter)
+        {
+            this.onConstraintCreated(new MatchesConstraint(predicate, descriptionWriter));
+            return default(T);
+        }
+
+        private class NotArgumentConstraintManager
+            : IArgumentConstraintManager<T>
+        {
+            private readonly IArgumentConstraintManager<T> parent;
+
+            public NotArgumentConstraintManager(IArgumentConstraintManager<T> parent)
+            {
+                this.parent = parent;
+            }
+
+            public IArgumentConstraintManager<T> Not
+            {
+                get { return new NotArgumentConstraintManager(this); }
+            }
+
+            public T Matches(Func<T, bool> predicate, Action<IOutputWriter> descriptionWriter)
+            {
+                return this.parent.Matches(
+                    x => !predicate(x), 
+                    x =>
+                    {
+                        x.Write("not ");
+                        descriptionWriter.Invoke(x);
+                    });
+            }
+        }
+
+        private class MatchesConstraint
+            : IArgumentConstraint2
+        {
+            private readonly Func<T, bool> predicate;
+            private readonly Action<IOutputWriter> descriptionWriter;
+
+            public MatchesConstraint(Func<T, bool> predicate, Action<IOutputWriter> descriptionWriter)
+            {
+                this.predicate = predicate;
+                this.descriptionWriter = descriptionWriter;
+            }
+
+            void IArgumentConstraint2.WriteDescription(IOutputWriter writer)
+            {
+                writer.Write("<");
+                this.descriptionWriter.Invoke(writer);
+                writer.Write(">");
+            }
+
+            bool IArgumentConstraint2.IsValid(object argument)
+            {
+                return this.predicate.Invoke((T)argument);
+            }
+        }
+    }
+}
