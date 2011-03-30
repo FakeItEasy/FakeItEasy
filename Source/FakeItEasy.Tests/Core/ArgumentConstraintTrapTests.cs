c75e0c6d75c287749ecd983c33e5097633539d58
patrik.hagne <devnull@localhost>
2011-03-30 11:27:26 +0200

    Implemented trapping of argument constraints.

---
+++ Source/FakeItEasy.Tests/Core/ArgumentConstraintTrapTests.cs
@@ -0,0 +1,57 @@
+﻿using FakeItEasy.Core;
+using NUnit.Framework;
+
+namespace FakeItEasy.Tests.Core
+{
+    [TestFixture]
+    public class ArgumentConstraintTrapTests
+    {
+        private ArgumentConstraintTrap trap = new ArgumentConstraintTrap();
+
+        [Test]
+        public void Should_return_constraints_that_has_been_trapped()
+        {
+            // Arrange
+            var constraint1 = A.Dummy<IArgumentConstraint>();
+            var constraint2 = A.Dummy<IArgumentConstraint>();
+
+            // Act
+            var result = this.trap.TrapConstraints(() =>
+                                                       {
+                                                           ArgumentConstraintTrap.ReportTrappedConstraint(constraint1);
+                                                           ArgumentConstraintTrap.ReportTrappedConstraint(constraint2);
+                                                       });
+
+            // Asssert
+            Assert.That(result, Is.EquivalentTo(new[] { constraint1, constraint2 }));
+        }
+
+        [Test]
+        public void Should_not_return_constraints_from_previous_call()
+        {
+            // Arrange
+            var constraint1 = A.Dummy<IArgumentConstraint>();
+            var constraint2 = A.Dummy<IArgumentConstraint>();
+
+            this.trap.TrapConstraints(() => ArgumentConstraintTrap.ReportTrappedConstraint(constraint1));
+
+            // Act
+            var result = this.trap.TrapConstraints(() =>
+                                                       {
+                                                           ArgumentConstraintTrap.ReportTrappedConstraint(constraint2);
+                                                       });
+
+            // Asssert
+            Assert.That(result, Is.EquivalentTo(new[] { constraint2 }));
+        }
+
+        [Test]
+        public void Should_not_fail_when_reporting_trapped_constraint_outside_call_to_trap_constraints()
+        {
+            // Arrange
+
+            // Act, Assert
+            Assert.DoesNotThrow(() => ArgumentConstraintTrap.ReportTrappedConstraint(A.Dummy<IArgumentConstraint>()));
+        }
+    }
+}
--- Source/FakeItEasy/Core/ArgumentConstraintTrap.cs
+++ Source/FakeItEasy/Core/ArgumentConstraintTrap.cs
@@ -1,13 +1,8 @@
-﻿namespace FakeItEasy.Core
+﻿using System;
+using System.Collections.Generic;
+
+namespace FakeItEasy.Core
 {
-    using System;
-    using System.Collections.Generic;
-
-    internal interface IArgumentConstraintTrapper
-    {
-        IEnumerable<IArgumentConstraint> TrapConstraints(Action actionThatProducesConstraint);
-    }
-
     internal class ArgumentConstraintTrap
         : IArgumentConstraintTrapper
     {
