da4245c312a53ffaf6d065b1f8cc76cb402a63c7
patrik@hagne.se <patrik@hagne.se>
2011-05-22 17:48:24 +0200

    Fixed so that the build script builds with and without strong name.

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,4 @@
 @echo off
-@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild FakeItEasy.build /property:SignAssembly=true %*
+@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild FakeItEasy.build %* 
+@%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild FakeItEasy.build /property:BuildPath=Build-Signed /property:SignAssembly=true %* 
 pause
