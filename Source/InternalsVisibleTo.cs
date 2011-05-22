384f690f519460cf6166fcc9768c849c3ff8eeef
patrik@hagne.se <patrik@hagne.se>
2011-05-22 13:00:40 +0200

    Added FxCop supressing for strong name rule.

--- Source/InternalsVisibleTo.cs
+++ Source/InternalsVisibleTo.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
+[module: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 [assembly: InternalsVisibleTo("FakeItEasy.Tests")]
