d4051dec6f0db1b11b78d5e8f0bda9d55aec21a1
patrik@hagne.se <patrik@hagne.se>
2011-06-11 16:40:52 +0200

    Implmented the auto initialization of suts from the DefaultFixtureInitializer.cs.

--- Source/FakeItEasy/Core/DefaultFixtureInitializer.cs
+++ Source/FakeItEasy/Core/DefaultFixtureInitializer.cs
@@ -1,6 +1,7 @@
 ﻿namespace FakeItEasy.Core
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
     using Creation;
@@ -9,28 +10,47 @@
         : IFixtureInitializer
     {
         private readonly IFakeAndDummyManager fakeAndDummyManager;
+        private readonly ISutInitializer sutInitializer;
 
-        public DefaultFixtureInitializer(IFakeAndDummyManager fakeAndDummyManager)
+        public DefaultFixtureInitializer(IFakeAndDummyManager fakeAndDummyManager, ISutInitializer sutInitializer)
         {
             this.fakeAndDummyManager = fakeAndDummyManager;
+            this.sutInitializer = sutInitializer;
         }
 
         public void InitializeFakes(object fixture)
         {
-            var settersForTaggedMembers =
-                from member in fixture.GetType().GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
-                where MemberIsTaggedWithFakeAttribute(member)
-                select GetSetterForMember(fixture, member);
+            var fakesUsedToCreateSut = this.InitializeSut(fixture);
 
-            foreach (var setter in settersForTaggedMembers)
-            {
-                var fake = this.fakeAndDummyManager.CreateFake(setter.MemberType, FakeOptions.Empty);
-
-                setter.Setter.Invoke(fake);
-            }
+            this.InitializeFakes(fixture, fakesUsedToCreateSut);
         }
 
-        private static SettableMemberInfo GetSetterForMember(object fixture, MemberInfo member)
+        private static SettableMemberInfo GetSutSetter(object fixture)
+        {
+            var allSettersTaggedUnderTest = GetMembersTaggedWithAttribute(fixture, typeof(UnderTestAttribute));
+            
+            if (CountIsNoMoreThanOne(allSettersTaggedUnderTest))
+            {
+                throw new InvalidOperationException("A fake fixture can only contain one member marked \"under test\".");
+            }
+
+            return allSettersTaggedUnderTest.SingleOrDefault();
+        }
+
+        private static bool CountIsNoMoreThanOne<T>(IEnumerable<T> collection)
+        {
+            return collection.Cast<object>().Skip(1).FirstOrDefault() != null;
+        }
+
+        private static IEnumerable<SettableMemberInfo> GetMembersTaggedWithAttribute(object getMembersFor, Type attributeType)
+        {
+            return
+                from member in getMembersFor.GetType().GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance)
+                where MemberIsTaggedWithAttribute(member, attributeType)
+                select GetSetterForMember(getMembersFor, member);
+        }
+
+        private static SettableMemberInfo GetSetterForMember(object instance, MemberInfo member)
         {
             var property = member as PropertyInfo;
             if (property != null)
@@ -38,7 +58,7 @@
                 return new SettableMemberInfo
                            {
                                MemberType = property.PropertyType, 
-                               Setter = x => property.GetSetMethod(nonPublic: true).Invoke(fixture, new[] { x })
+                               Setter = x => property.GetSetMethod(nonPublic: true).Invoke(instance, new[] { x })
                            };
             }
 
@@ -48,17 +68,49 @@
                 return new SettableMemberInfo
                            {
                                MemberType = field.FieldType, 
-                               Setter = x => field.SetValue(fixture, x)
+                               Setter = x => field.SetValue(instance, x)
                            };
             }
 
             return null;
         }
 
-        private static bool MemberIsTaggedWithFakeAttribute(MemberInfo member)
+        private static bool MemberIsTaggedWithAttribute(MemberInfo member, Type attributeType)
         {
-            return (from attribute in member.GetCustomAttributes(typeof(FakeAttribute), false)
+            return (from attribute in member.GetCustomAttributes(attributeType, false)
                     select attribute).Any();
+        }
+
+        private void InitializeFakes(object fixture, Dictionary<Type, object> fakesUsedToCreateSut)
+        {
+            var settersForTaggedMembers = GetMembersTaggedWithAttribute(fixture, typeof(FakeAttribute));
+
+            foreach (var setter in settersForTaggedMembers)
+            {
+                object fake = null;
+
+                if (!fakesUsedToCreateSut.TryGetValue(setter.MemberType, out fake))
+                {
+                    fake = this.fakeAndDummyManager.CreateFake(setter.MemberType, FakeOptions.Empty);
+                }
+
+                setter.Setter(fake);
+            }
+        }
+
+        private Dictionary<Type, object> InitializeSut(object fixture)
+        {
+            var result = new Dictionary<Type, object>();
+
+            var setter = GetSutSetter(fixture);
+            if (setter != null)
+            {
+
+                var sut = this.sutInitializer.CreateSut(setter.MemberType, result.Add);
+                setter.Setter(sut);
+            }
+
+            return result;
         }
 
         private class SettableMemberInfo
