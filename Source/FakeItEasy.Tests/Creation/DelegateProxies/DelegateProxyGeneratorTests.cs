745f510bd53b149bdfc013a91cb053b7ab505550
Patrik Hägne <patrik@hagne.se>
2011-07-15 13:14:05 +0200

    Implemented proxy generation for delegates.

---
+++ Source/FakeItEasy.Tests/Creation/DelegateProxies/DelegateProxyGeneratorTests.cs
@@ -0,0 +1,196 @@
+﻿namespace FakeItEasy.Tests.Creation.DelegateProxies
+{
+    using System;
+    using System.Linq;
+    using FakeItEasy.Core;
+    using FakeItEasy.Creation;
+    using NUnit.Framework;
+    using FakeItEasy.Creation.DelegateProxies;
+    using System.Reflection;
+
+    [TestFixture]
+    public class DelegateProxyGeneratorTests
+    {
+        [UnderTest] DelegateProxyGenerator generator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Fake.InitializeFixture(this);
+        }
+
+        [TestCase(typeof(Func<int>))]
+        [TestCase(typeof(Action))]
+        [TestCase(typeof(EventHandler<EventArgs>))]
+        [TestCase(typeof(Predicate<object>))]
+        public void Should_be_successfull_with_delegate_types(Type delegateType)
+        {
+            // Arrange
+
+            // Act
+            var result = this.generator.GenerateProxy(delegateType, Enumerable.Empty<Type>(), Enumerable.Empty<object>());
+
+            // Assert
+            Assert.That(result.ProxyWasSuccessfullyGenerated, Is.True);
+        }
+
+        [TestCase(typeof(object))]
+        [TestCase(typeof(string))]
+        [TestCase(typeof(IServiceProvider))]
+        [SetCulture("en-US")]
+        public void Should_fail_for_non_delegate_types(Type nonDelegateType)
+        {
+            // Arrange
+
+            // Act
+            var result = this.generator.GenerateProxy(nonDelegateType, Enumerable.Empty<Type>(), Enumerable.Empty<object>());
+            
+            // Assert
+            Assert.That(result.ProxyWasSuccessfullyGenerated, Is.False);
+            Assert.That(result.ReasonForFailure, Is.EqualTo("The delegate proxy generator can only create proxies for delegate types."));
+        }
+
+        [TestCase(typeof(Func<int>))]
+        [TestCase(typeof(Action))]
+        [TestCase(typeof(EventHandler<EventArgs>))]
+        [TestCase(typeof(Predicate<object>))]
+        public void Should_create_proxy_of_the_specified_type(Type delegateType)
+        {
+            // Arrange
+
+            // Act
+            var result = this.generator.GenerateProxy(delegateType, Enumerable.Empty<Type>(), Enumerable.Empty<object>());
+
+            // Assert
+            Assert.That(result.GeneratedProxy.GetType(), Is.EqualTo(delegateType));
+        }
+
+        [Test]
+        public void Should_return_proxy_where_return_value_can_be_set()
+        {
+            // Arrange
+            var proxy = this.GenerateProxy<Func<int>>(x => x.SetReturnValue(10));
+
+            // Act
+            var result = proxy.Invoke();
+
+            // Assert
+            Assert.That(result, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void Should_return_proxy_that_raises_event_with_arguments_passed_to_proxy()
+        {
+            // Arrange
+            int firstArgument = 0;
+            string secondArgument = null;
+            
+            var proxy = this.GenerateProxy<Action<int, string>>(x =>
+                {
+                    firstArgument = x.Arguments.Get<int>(0);
+                    secondArgument = x.Arguments.Get<string>(1);
+                });
+
+            // Act
+            proxy.Invoke(10, "foo");
+
+            // Assert
+            Assert.That(firstArgument, Is.EqualTo(10));
+            Assert.That(secondArgument, Is.EqualTo("foo"));
+        }
+
+        [Test]
+        public void Should_return_proxy_that_can_not_be_configured_to_call_base_method()
+        {
+            // Arrange
+            var proxy = this.GenerateProxy<Action>(x =>
+                {
+                    var ex = Assert.Throws<NotSupportedException>(x.CallBaseMethod);
+                    Assert.That(ex.Message, Is.EqualTo("Can not configure a delegate proxy to call base method."));
+                });
+
+            // Act
+            proxy.Invoke();
+        }
+
+        [Test]
+        public void Should_create_calls_that_exposes_return_value_when_converted_to_read_only()
+        {
+            // Arrange
+            var proxy = this.GenerateProxy<Func<string>>(x =>
+                {
+                    x.SetReturnValue("foo");
+
+                    Assert.That(x.AsReadOnly().ReturnValue, Is.EqualTo("foo"));
+                });
+
+            // Act
+            proxy.Invoke();
+        }
+
+        [Test, Ignore]
+        public void Should_return_proxy_where_out_parameter_can_be_set()
+        {
+            // Arrange
+            var proxy = this.GenerateProxy<DelegateWithOutValue>(x =>
+                {
+                    x.SetArgumentValue(0, "Foo");
+                });
+
+            // Act
+            string output = null;
+            proxy.Invoke(out output);
+
+            // Assert
+            Assert.That(output, Is.EqualTo("Foo"));
+        }
+
+        [Test]
+        public void Should_return_proxy_where_calls_has_the_correct_method_set()
+        {
+            // Arrange
+            var expectedMethod = typeof(Action).GetMethod("Invoke");
+            MethodInfo actualMethod = null;
+
+            var proxy = this.GenerateProxy<Action>(x =>
+                {
+                    actualMethod = x.Method;
+                });
+
+            // Act
+            proxy.Invoke();
+
+            // Assert
+            Assert.That(expectedMethod, Is.EqualTo(actualMethod));
+        }
+
+        [Test]
+        public void Should_return_proxy_where_calls_has_the_correct_faked_object_set()
+        {
+            // Arrange
+            object fakedObject = null;
+
+            var proxy = this.GenerateProxy<Action>(x =>
+                {
+                    fakedObject = x.FakedObject;
+                });
+
+            // Act
+            proxy.Invoke();
+
+            // Assert
+            Assert.That(fakedObject, Is.SameAs(proxy));
+        }
+
+        public delegate void DelegateWithOutValue(out string result);
+
+        private T GenerateProxy<T>(Action<IWritableFakeObjectCall> callInterceptor)
+        {
+            var result = this.generator.GenerateProxy(typeof (T), Enumerable.Empty<Type>(), Enumerable.Empty<object>());
+
+            result.CallInterceptedEventRaiser.CallWasIntercepted += (_, e) => callInterceptor(e.Call);
+
+            return (T)result.GeneratedProxy;
+        }
+    }
+}
---
+++ Source/FakeItEasy/Creation/DelegateProxies/DelegateProxyGenerator.cs
@@ -0,0 +1,142 @@
+﻿namespace FakeItEasy.Creation.DelegateProxies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    using FakeItEasy.Core;
+
+    internal class DelegateProxyGenerator
+        : IProxyGenerator
+    {
+        public ProxyGeneratorResult GenerateProxy(Type typeOfProxy, IEnumerable<Type> additionalInterfacesToImplement,
+                                                  IEnumerable<object> argumentsForConstructor)
+        {
+            if (!typeof (Delegate).IsAssignableFrom(typeOfProxy))
+            {
+                return
+                    new ProxyGeneratorResult("The delegate proxy generator can only create proxies for delegate types.");
+            }
+
+
+            var invokeMethod = typeOfProxy.GetMethod("Invoke");
+
+            var eventRaiser = new DelegateCallInterceptedEventRaiser();
+            
+            var proxy = CreateDelegateProxy(typeOfProxy, invokeMethod, eventRaiser);
+            
+            eventRaiser.Method = invokeMethod;
+            eventRaiser.Instance = proxy;
+
+            return new ProxyGeneratorResult(proxy, eventRaiser);
+        }
+
+        public bool MethodCanBeInterceptedOnInstance(MethodInfo method, object callTarget, out string failReason)
+        {
+            throw new NotImplementedException();
+        }
+
+        static Delegate CreateDelegateProxy(Type typeOfProxy, MethodInfo invokeMethod, DelegateCallInterceptedEventRaiser eventRaiser)
+        {
+            var delegateMethod = invokeMethod;
+
+            var parameterExpressions =
+                delegateMethod.GetParameters().Select(x => Expression.Parameter(x.ParameterType, x.Name)).ToArray();
+
+            var body = CreateBodyExpression(delegateMethod, eventRaiser, parameterExpressions);
+
+            return Expression.Lambda(typeOfProxy,
+                                     body,
+                                     parameterExpressions).Compile();
+        }
+
+        static Expression CreateBodyExpression(MethodInfo delegateMethod, DelegateCallInterceptedEventRaiser eventRaiser, Expression[] parameterExpressions)
+        {
+            var parameterExpressionsCastToObject =
+                parameterExpressions.Select(x => Expression.Convert(x, typeof(object))).Cast<Expression>().ToArray();
+
+            Expression body = Expression.Call(
+                Expression.Constant(eventRaiser),
+                DelegateCallInterceptedEventRaiser.RaiseMethod,
+                new Expression[]
+                    {
+                        Expression.NewArrayInit(typeof (object), parameterExpressionsCastToObject)
+                    });
+
+            if (!delegateMethod.ReturnType.Equals(typeof (void)))
+            {
+                body = Expression.Convert(body, delegateMethod.ReturnType);
+            }
+
+            return body;
+        }
+
+        class DelegateCallInterceptedEventRaiser : ICallInterceptedEventRaiser
+        {
+            public static readonly MethodInfo RaiseMethod =
+                typeof (DelegateCallInterceptedEventRaiser).GetMethod("Raise");
+
+            public MethodInfo Method { get; set; }
+
+            public Delegate Instance { get; set; }
+
+            public event EventHandler<CallInterceptedEventArgs> CallWasIntercepted;
+
+            public object Raise(object[] arguments)
+            {
+                var call = new DelegateFakeObjectCall(this.Instance, this.Method, arguments);
+                EventHandler<CallInterceptedEventArgs> handler = this.CallWasIntercepted;
+
+                if (handler != null)
+                    this.CallWasIntercepted(null, new CallInterceptedEventArgs(call));
+
+                return call.ReturnValue;
+            }
+        }
+
+        class DelegateFakeObjectCall
+            : IWritableFakeObjectCall, ICompletedFakeObjectCall
+        {
+            readonly object instance;
+
+            public DelegateFakeObjectCall(object instance, MethodInfo method, object[] arguments)
+            {
+                this.instance = instance;
+                this.Arguments = new ArgumentCollection(arguments, method);
+                this.Method = method;
+            }
+
+            public object ReturnValue { get; set; }
+
+            public void SetReturnValue(object value)
+            {
+                this.ReturnValue = value;
+            }
+
+            public void CallBaseMethod()
+            {
+                throw new NotSupportedException("Can not configure a delegate proxy to call base method.");
+            }
+
+            public void SetArgumentValue(int index, object value)
+            {
+                this.Arguments.GetUnderlyingArgumentsArray()[index] = value;
+            }
+
+            public ICompletedFakeObjectCall AsReadOnly()
+            {
+                return this;
+            }
+
+            public MethodInfo Method { get; set; }
+
+            public ArgumentCollection Arguments { get; set; }
+
+            public object FakedObject
+            {
+                get { return this.instance; }
+            }
+        }
+    }
+}
