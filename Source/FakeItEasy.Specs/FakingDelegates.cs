4a8a3d3ca8a85a07361780ae22e650f85cb06118
Patrik Hägne <patrik@hagne.se>
2011-07-15 17:29:29 +0200

    Added specs for delegate proxies.

---
+++ Source/FakeItEasy.Specs/FakingDelegates.cs
@@ -0,0 +1,33 @@
+﻿namespace FakeItEasy.IntegrationTests
+{
+    using System;
+    using Machine.Specifications;
+
+    public class when_faking_a_delegate_type
+    {
+        static Func<string, int> fakedDelegate;
+
+        Because of = () => fakedDelegate = A.Fake<Func<string, int>>();
+
+        It should_be_able_to_intercept_call = () =>
+            {
+                fakedDelegate.Invoke("foo");
+                A.CallTo(() => fakedDelegate("foo")).MustHaveHappened();
+            };
+
+        It should_be_able_to_set_return_value = () =>
+            {
+                A.CallTo(() => fakedDelegate(A<string>._)).Returns(10);
+                fakedDelegate(null).ShouldEqual(10);
+            };
+
+        It should_be_able_to_configure_delegate_to_throw = () =>
+            {
+                var expectedException = new FormatException();
+
+                A.CallTo(() => fakedDelegate(A<string>._)).Throws(expectedException);
+
+                typeof (FormatException).ShouldBeThrownBy(() => fakedDelegate(null));
+            };
+    }
+}
