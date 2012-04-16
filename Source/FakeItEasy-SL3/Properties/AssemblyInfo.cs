7d34d51a5febd7ebc7dcc7231b8630cba75b3447
Patrik Hagne <patrik@hagne.se>
2012-04-16 14:39:17 +0200

    Started to change the build script.

---
+++ Source/FakeItEasy-SL3/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FakeItEasy Silverlight")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c153002f-befa-4cfa-bc25-3a1797dc73fb")]
