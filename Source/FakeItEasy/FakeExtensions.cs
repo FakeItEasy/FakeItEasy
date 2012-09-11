9061a6b666fe25a1e73d794f25ed225065ba467c
philippdolder <philipp.dolder@gmail.com>
2012-09-11 14:26:18 +0200

    Throws: Extended Throws syntax to support easy access for up to four arguments

--- Source/FakeItEasy/FakeExtensions.cs
+++ Source/FakeItEasy/FakeExtensions.cs
@@ -6,10 +6,11 @@ namespace FakeItEasy
     using System.Linq;
     using System.Linq.Expressions;
     using System.Reflection;
-    using Creation;
-    using Configuration;
-    using Core;
-    using Expressions;
+
+    using FakeItEasy.Configuration;
+    using FakeItEasy.Core;
+    using FakeItEasy.Creation;
+    using FakeItEasy.Expressions;
 
     /// <summary>
     /// Provides extension methods for fake objects.
@@ -19,6 +20,8 @@ namespace FakeItEasy
         private const string NameOfInvokesFeature = "invokes";
 
         private const string NameOfReturnsLazilyFeature = "returns lazily";
+
+        private const string NameOfThrowsFeature = "throws";
 
         /// <summary>
         /// Specifies NumberOfTimes(1) to the IRepeatConfiguration{TFake}.
@@ -446,6 +449,88 @@ namespace FakeItEasy
         public static IAfterCallSpecifiedConfiguration Throws(this IExceptionThrowerConfiguration configuration, Func<Exception> exceptionFactory)
         {
             return configuration.Throws(_ => exceptionFactory());
+        }
+
+        /// <summary>
+        /// Throws the specified exception when the currently configured
+        /// call gets called.
+        /// </summary>
+        /// <param name="configuration">The configuration to use.</param>
+        /// <param name="exceptionFactory">A function that returns the exception to throw when invoked.</param>
+        /// <typeparam name="T1">Type of the first argument of the faked method call</typeparam>
+        /// <returns>Configuration object.</returns>
+        /// <exception cref="FakeConfigurationException"> when the signatures of the faked method and the <paramref name="exceptionFactory"/> do not match</exception>
+        public static IAfterCallSpecifiedConfiguration Throws<T1>(this IExceptionThrowerConfiguration configuration, Func<T1, Exception> exceptionFactory)
+        {
+            return configuration.Throws(call =>
+                {
+                    AssertThatSignaturesAreEqual(call.Method, exceptionFactory.Method, NameOfThrowsFeature);
+
+                    return exceptionFactory(call.GetArgument<T1>(0));
+                });
+        }
+
+        /// <summary>
+        /// Throws the specified exception when the currently configured
+        /// call gets called.
+        /// </summary>
+        /// <param name="configuration">The configuration to use.</param>
+        /// <param name="exceptionFactory">A function that returns the exception to throw when invoked.</param>
+        /// <typeparam name="T1">Type of the first argument of the faked method call</typeparam>
+        /// <typeparam name="T2">Type of the second argument of the faked method call</typeparam>
+        /// <returns>Configuration object.</returns>
+        /// <exception cref="FakeConfigurationException"> when the signatures of the faked method and the <paramref name="exceptionFactory"/> do not match</exception>
+        public static IAfterCallSpecifiedConfiguration Throws<T1, T2>(this IExceptionThrowerConfiguration configuration, Func<T1, T2, Exception> exceptionFactory)
+        {
+            return configuration.Throws(call =>
+                {
+                    AssertThatSignaturesAreEqual(call.Method, exceptionFactory.Method, NameOfThrowsFeature);
+
+                    return exceptionFactory(call.GetArgument<T1>(0), call.GetArgument<T2>(1));
+                });
+        }
+
+        /// <summary>
+        /// Throws the specified exception when the currently configured
+        /// call gets called.
+        /// </summary>
+        /// <param name="configuration">The configuration to use.</param>
+        /// <param name="exceptionFactory">A function that returns the exception to throw when invoked.</param>
+        /// <typeparam name="T1">Type of the first argument of the faked method call</typeparam>
+        /// <typeparam name="T2">Type of the second argument of the faked method call</typeparam>
+        /// <typeparam name="T3">Type of the third argument of the faked method call</typeparam>
+        /// <returns>Configuration object.</returns>
+        /// <exception cref="FakeConfigurationException"> when the signatures of the faked method and the <paramref name="exceptionFactory"/> do not match</exception>
+        public static IAfterCallSpecifiedConfiguration Throws<T1, T2, T3>(this IExceptionThrowerConfiguration configuration, Func<T1, T2, T3, Exception> exceptionFactory)
+        {
+            return configuration.Throws(call =>
+                {
+                    AssertThatSignaturesAreEqual(call.Method, exceptionFactory.Method, NameOfThrowsFeature);
+
+                    return exceptionFactory(call.GetArgument<T1>(0), call.GetArgument<T2>(1), call.GetArgument<T3>(2));
+                });
+        }
+
+        /// <summary>
+        /// Throws the specified exception when the currently configured
+        /// call gets called.
+        /// </summary>
+        /// <param name="configuration">The configuration to use.</param>
+        /// <param name="exceptionFactory">A function that returns the exception to throw when invoked.</param>
+        /// <typeparam name="T1">Type of the first argument of the faked method call</typeparam>
+        /// <typeparam name="T2">Type of the second argument of the faked method call</typeparam>
+        /// <typeparam name="T3">Type of the third argument of the faked method call</typeparam>
+        /// <typeparam name="T4">Type of the fourth argument of the faked method call</typeparam>
+        /// <returns>Configuration object.</returns>
+        /// <exception cref="FakeConfigurationException"> when the signatures of the faked method and the <paramref name="exceptionFactory"/> do not match</exception>
+        public static IAfterCallSpecifiedConfiguration Throws<T1, T2, T3, T4>(this IExceptionThrowerConfiguration configuration, Func<T1, T2, T3, T4, Exception> exceptionFactory)
+        {
+            return configuration.Throws(call =>
+                {
+                    AssertThatSignaturesAreEqual(call.Method, exceptionFactory.Method, NameOfThrowsFeature);
+
+                    return exceptionFactory(call.GetArgument<T1>(0), call.GetArgument<T2>(1), call.GetArgument<T3>(2), call.GetArgument<T4>(3));
+                });
         }
 
         /// <summary>
