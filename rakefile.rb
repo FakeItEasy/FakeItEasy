4e38f9a7e936ce2ee71610951969d93deddad619
Matt Jackson <matt.jackson@credit-suisse.com>
2013-02-19 17:55:49 +0100

    #16 - merge assemblies using ilrepack
    - added nuget package for ilrepack
    - added rake tasks for merging projects
    - updated nuspec to pull from merged paths

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,7 @@ require 'fileutils'
 version = IO.read("Source/Version.txt")
 nunit_command = "Source/packages/NUnit.Runners.2.6.2/tools/nunit-console.exe"
 mspec_command = "Source/packages/Machine.Specifications.0.5.8/tools/mspec-clr4.exe"
+ilrepack_command = "Source/packages/ILRepack.1.21.3/lib/ilrepack.exe"
 nuget_command = "Source/.nuget/NuGet.exe"
 
 Albacore.configure do |config|
@@ -58,8 +59,32 @@ mspec :spec => [:build] do |mspec|
 end
 
 desc "create the nuget package"
-exec :pack => [:build] do |cmd|
+exec :pack => [:build, :ilrepack, :ilrepack_35, :ilrepack_sl] do |cmd|
   FileUtils.mkpath "Build"
   cmd.command = nuget_command
   cmd.parameters "pack Source/FakeItEasy.nuspec -Version " + version + " -OutputDirectory Build"
+end
+
+desc "merge dependencies"
+exec :ilrepack do |cmd|
+  assemblies = "Source/FakeItEasy/Bin/Release/FakeItEasy.dll Source/FakeItEasy/Bin/Release/Castle.Core.dll"
+
+  cmd.command = ilrepack_command
+  cmd.parameters = "/internalize /out:Source/FakeItEasy/Bin/Release/Merged/FakeItEasy.dll /xmldocs /targetplatform:v4 /log:Source/FakeItEasy/Bin/Release/Merged/merge.log.txt #{ assemblies } "
+end
+
+desc "merge 3.5 dependencies"
+exec :ilrepack_35 do |cmd|
+  assemblies = "Source/FakeItEasy.Net35/Bin/Release/FakeItEasy.dll Source/FakeItEasy.Net35/Bin/Release/Castle.Core.dll"
+
+  cmd.command = ilrepack_command
+  cmd.parameters = "/internalize /out:Source/FakeItEasy.Net35/Bin/Release/Merged/FakeItEasy.dll /xmldocs /targetplatform:v4 /log:Source/FakeItEasy.Net35/Bin/Release/Merged/merge.log.txt #{ assemblies } "
+end
+
+desc "merge SL dependencies"
+exec :ilrepack_sl do |cmd|
+  assemblies = "Source/FakeItEasy-SL/Bin/Release/FakeItEasy.dll Source/FakeItEasy-SL/Bin/Release/Castle.Core.dll"
+
+  cmd.command = ilrepack_command
+  cmd.parameters = "/internalize /out:Source/FakeItEasy-SL/Bin/Release/Merged/FakeItEasy.dll /xmldocs /targetplatform:v4 /log:Source/FakeItEasy-SL/Bin/Release/Merged/merge.log.txt #{ assemblies } "
 end
