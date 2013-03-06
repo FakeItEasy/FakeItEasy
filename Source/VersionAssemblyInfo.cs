10745ce86a5daa20804d8bdabcadbe0814876c40
Adam Ralph <adam@adamralph.com>
2013-03-06 16:29:54 +0100

    #74 updated version and release notes

--- Source/VersionAssemblyInfo.cs
+++ Source/VersionAssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-[assembly: AssemblyVersion("1.9.0")]
-[assembly: AssemblyFileVersion("1.9.0")]
+[assembly: AssemblyVersion("1.9.1")]
+[assembly: AssemblyFileVersion("1.9.1")]
 
-[assembly: AssemblyInformationalVersion("1.9.0")]
-[assembly: AssemblyConfiguration("Release built on 2013-03-04 21:14:17+0100")]
+[assembly: AssemblyInformationalVersion("1.9.1")]
+[assembly: AssemblyConfiguration("Release built on 2013-03-06 16:21:47+0100")]
