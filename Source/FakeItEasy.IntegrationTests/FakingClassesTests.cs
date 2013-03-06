0e237ea52d69f4409687be3792c7212489d4ebed
Adam Ralph <adam@adamralph.com>
2013-03-06 16:14:26 +0100

    #74 add failing test

--- Source/FakeItEasy.IntegrationTests/FakingClassesTests.cs
+++ Source/FakeItEasy.IntegrationTests/FakingClassesTests.cs
@@ -2,6 +2,8 @@ namespace FakeItEasy.IntegrationTests
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.IO;
+    using System.Runtime.Serialization.Formatters.Binary;
     using Core;
     using NUnit.Framework;
 
@@ -15,6 +17,25 @@ namespace FakeItEasy.IntegrationTests
             {
                 A.Fake<Uri>();
             }
+        }
+
+        [Test]
+        public void Should_be_able_to_serialize_a_fake()
+        {
+            // arrange
+            var person = A.Fake<Person>();
+
+            var formatter = new BinaryFormatter();
+            using (var stream = new MemoryStream())
+            {
+                // act
+                formatter.Serialize(stream, person);
+            }
+        }
+
+        [Serializable]
+        public class Person
+        {
         }
     }
 
