d95932359d8ca601c9e4bd3687ecf914b08f94d9
Adam Ralph <adam@adamralph.com>
2013-03-28 21:34:35 +0100

    re #67 changed version to 1.10, updated release notes and added iconUrl

--- Source/VersionAssemblyInfo.cs
+++ Source/VersionAssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.10.0")]
 [assembly: AssemblyFileVersion("1.10.0")]
-[assembly: AssemblyInformationalVersion("1.10.0-beta02")]
-[assembly: AssemblyConfiguration("Release built on 2013-03-06 17:01:16+0100")]
+[assembly: AssemblyInformationalVersion("1.10.0")]
+[assembly: AssemblyConfiguration("Release built on 2013-03-28 21:29:45+0100")]
