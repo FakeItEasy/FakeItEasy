d0bb35ad6a329a00472057f8dc62a358e7f1568a
Blair Conrad <blair.conrad@gmail.com>
2013-08-02 22:16:27 -0400

    removed VersionAssemblyInfo.cs, Version.txt to fix #127

--- Source/VersionAssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("1.13.1")]
-[assembly: AssemblyFileVersion("1.13.1")]
-[assembly: AssemblyInformationalVersion("1.13.1")]
-[assembly: AssemblyConfiguration("Release built on 2013-07-30 22:29:00+0200")]
