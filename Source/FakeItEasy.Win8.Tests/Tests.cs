0ae4dba45d4101167df64f81e0b6fc6699ab6897
Adam Ralph <adam@adamralph.com>
2014-01-07 22:21:59 +0100

    #214 added Win8 test project referencing SL5 FIE

---
+++ Source/FakeItEasy.Win8.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace FakeItEasy.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+
+    public static class Tests
+    {
+        [Test]
+        public static void CanUseFakeItEasy()
+        {
+            var fake = A.Fake<IDummyDefinition>();
+            A.CallTo(() => fake.CreateDummy()).Returns(new object());
+            fake.CreateDummy().Should().NotBeNull();
+        }
+    }
+}
