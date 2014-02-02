ad9a97fb6c873a18b61c752caba21813e3b72b40
Blair Conrad <blair.conrad@gmail.com>
2014-02-02 10:02:30 -0500

    extracted StringBuilderExtensions

--- Source/FakeItEasy/CommonExtensions.cs
+++ Source/FakeItEasy/CommonExtensions.cs
@@ -66,24 +66,6 @@
             return sequence.ToLookup(keySelector).ToDictionary(x => x.Key, x => x.First());
         }
 
-        public static StringBuilder AppendIndented(this StringBuilder builder, string indentString, string value)
-        {
-            var lines = value == null ? new string[] { } : value.Split('\n');
-
-            for (var i = 0; i < lines.Length; i++)
-            {
-                var line = lines[i];
-                builder.Append(indentString).Append(line);
-
-                if (i != lines.Length - 1)
-                {
-                    builder.Append('\n');
-                }
-            }
-
-            return builder;
-        }
-
         public static string GetGenericArgumentsCSharp(this MethodBase method)
         {
             var genericArguments = method.GetGenericArguments();
