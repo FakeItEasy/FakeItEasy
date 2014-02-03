07bb87adeecaf46eca31574bf6b3402bd00f36bd
Blair Conrad <blair.conrad@gmail.com>
2014-02-02 22:30:29 -0500

    merged WriteExtensions into FakeObjectCallExtensions

--- Source/FakeItEasy.Tests/FakeObjectCallExtensionsTests.cs
+++ Source/FakeItEasy.Tests/FakeObjectCallExtensionsTests.cs
@@ -1,5 +1,8 @@
 namespace FakeItEasy.Tests
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     using FakeItEasy.Core;
     using FluentAssertions;
     using NUnit.Framework;
@@ -111,6 +114,75 @@ namespace FakeItEasy.Tests
 
             // Assert
             description.Should().Be("FakeItEasy.Tests.IFoo.Bar(<string.Empty>, 123)");
+        }
+
+        [Test]
+        public void Write_should_be_null_guarded()
+        {
+            NullGuardedConstraint.Assert(() =>
+                Enumerable.Empty<IFakeObjectCall>().Write(A.Dummy<IOutputWriter>()));
+        }
+
+        [Test]
+        public void Write_should_call_writer_registered_in_container_with_calls()
+        {
+            // Arrange
+            var calls = A.CollectionOfFake<IFakeObjectCall>(2);
+
+            var callWriter = A.Fake<CallWriter>();
+            this.StubResolve<CallWriter>(callWriter);
+
+            var writer = A.Dummy<IOutputWriter>();
+
+            // Act
+            calls.Write(writer);
+
+            // Assert
+            A.CallTo(() => callWriter.WriteCalls(calls, writer)).MustHaveHappened();
+        }
+
+        [Test]
+        public void WriteToConsole_should_be_null_guarded()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            NullGuardedConstraint.Assert(() =>
+                Enumerable.Empty<IFakeObjectCall>().WriteToConsole());
+        }
+
+        [Test]
+        public void WriteToConsole_should_call_writer_registered_in_container_with_calls()
+        {
+            // Arrange
+            var calls = A.CollectionOfFake<IFakeObjectCall>(2);
+
+            var callWriter = A.Fake<CallWriter>();
+            this.StubResolve<CallWriter>(callWriter);
+
+            // Act
+            calls.WriteToConsole();
+
+            // Assert
+            A.CallTo(() => callWriter.WriteCalls(calls, A<IOutputWriter>._)).MustHaveHappened();
+        }
+
+        [Test]
+        public void WriteToConsole_should_call_writer_registered_in_container_with_console_out()
+        {
+            // Arrange
+            var calls = A.CollectionOfFake<IFakeObjectCall>(2);
+
+            var callWriter = A.Fake<CallWriter>();
+            this.StubResolve<CallWriter>(callWriter);
+
+            // Act
+            calls.WriteToConsole();
+
+            // Assert
+            A.CallTo(() => callWriter.WriteCalls(A<IEnumerable<IFakeObjectCall>>._, A<IOutputWriter>._)).MustHaveHappened();
         }
 
         private static FakeCall CreateFakeCallToFooDotBar(object argument1, object argument2)
