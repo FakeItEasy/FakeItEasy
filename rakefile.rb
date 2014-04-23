24cc02fee0948d312b642466a741b3eb97bb4d6d
Blair Conrad <blair@blairconrad.com>
2014-04-22 21:30:43 -0400

    #294 - auto-downloading SSL cert file if needed and printing progress messages

--- rakefile.rb
+++ rakefile.rb
@@ -65,6 +65,8 @@ With special thanks for contributions to this release from:
 * _&lt;user's actual name&gt;_ - _@&lt;github_userid&gt;_
 eos
 
+ssl_cert_file_url = "http://curl.haxx.se/ca/cacert.pem"
+
 Albacore.configure do |config|
   config.log_level = :verbose
 end
@@ -74,16 +76,16 @@ task :default => [ :vars, :unit, :integ, :spec, :pack ]
 
 desc "Print all variables"
 task :vars do
-  puts "nuget_command: #{nuget_command}"
-  puts "nunit_command: #{nunit_command}"
-  puts "mspec_command: #{mspec_command}"
-  
-  puts "solution:      #{solution}"
-  puts "assembly_info: #{assembly_info}"
-  puts "version:       #{version}"
-  puts "nuspec:        #{nuspec}"
-  puts "output_folder: #{output_folder}"
-  puts "repo:          #{repo}"
+  puts "nuget_command:     #{nuget_command}"
+  puts "nunit_command:     #{nunit_command}"
+  puts "mspec_command:     #{mspec_command}"
+  puts "solution:          #{solution}"
+  puts "assembly_info:     #{assembly_info}"
+  puts "version:           #{version}"
+  puts "nuspec:            #{nuspec}"
+  puts "output_folder:     #{output_folder}"
+  puts "repo:              #{repo}"
+  puts "ssl_cert_file_url: #{ssl_cert_file_url}"
   puts ""
 
   put_var_array("unit_tests", unit_tests)
@@ -162,24 +164,32 @@ end
 desc "create new milestone, release issue and release"
 task :create_milestone, :milestone_version do |t, args|
   require 'octokit'
+
+  ssl_cert_file = get_temp_ssl_cert_file(ssl_cert_file_url)
+
   client = Octokit::Client.new(:netrc => true)
 
   release_description = args.milestone_version + ' release'
 
+  puts "Creating milestone #{args.milestone_version}..."
   milestone = client.create_milestone(
     repo,
     args.milestone_version,
     :description => release_description
     )
+  puts "Created milestone #{args.milestone_version}..."
 
-  client.create_issue(
+  puts "Creating issue '#{release_description}'..."
+  issue = client.create_issue(
     repo,
     release_description,
     release_issue_body,
     :labels => release_issue_labels,
     :milestone => milestone.number
     )
+  puts "Created issue \##{issue.number} '#{release_description}'."
 
+  puts "Creating release '#{args.milestone_version}'..."
   client.create_release(
     repo,
     args.milestone_version,
@@ -187,10 +197,39 @@ task :create_milestone, :milestone_version do |t, args|
     :draft => true,
     :body => release_body
     )
+  puts "Created release '#{args.milestone_version}'."
 end
 
 def put_var_array(name, values)
   puts "#{name}:"
   puts values.map {|x| "  " + x }
   puts ""
+end
+
+# Get a temporary SSL cert file if necessary.
+# If ENV["SSL_CERT_FILE"] is set, will return nil.
+# Otherwise, attempts to download a known
+# SSL cert file, sets ENV["SSL_CERT_FILE"]
+# to point at it, and returns the file (mostly so it will
+# stay in scope while it's needed).
+def get_temp_ssl_cert_file(ssl_cert_file_url)
+  ssl_cert_file_path = ENV["SSL_CERT_FILE"]
+  if ssl_cert_file_path
+    return nil
+  end
+  
+  puts "Environment variable SSL_CERT_FILE is not set. Attempting to download a cert file..."
+
+  require 'open-uri'
+  require 'tempfile'
+
+  file = Tempfile.new('ssl_cert_file')
+  file.binmode
+  file << open(ssl_cert_file_url).read
+  file.close
+
+  ENV["SSL_CERT_FILE"] = file.path
+
+  puts "Downloaded cert file to #{ENV['SSL_CERT_FILE']}."
+  return file
 end
