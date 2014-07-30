4d663e73681326fcd3b2d78b7ff6f5dedd52c1ab
Blair Conrad <blair@blairconrad.com>
2014-07-29 20:34:27 -0400

    #338 - added a failing spec

--- Source/FakeItEasy.Specs/FakingInternalsSpecs.cs
+++ Source/FakeItEasy.Specs/FakingInternalsSpecs.cs
@@ -2,11 +2,21 @@
 {
     using System;
     using System.Collections.Generic;
+    using FakeItEasy.Configuration;
+    using FakeItEasy.Tests.TestHelpers;
     using FluentAssertions;
     using Machine.Specifications;
 
     internal interface IInternal
     {
+    }
+
+    public class TypeWithInternalMethod
+    {
+        internal virtual int InternalMethod()
+        {
+            return 8;
+        }
     }
 
     public class when_trying_to_fake_invisible_internals
@@ -35,5 +45,20 @@
                     "No usable default constructor was found on the type System.Collections.Generic.IList`1[FakeItEasy.Specs.IInternal]")
                 .And.Contain(
                     "because type FakeItEasy.Specs.IInternal is not accessible. Make it public, or internal and mark your assembly with [assembly: InternalsVisibleTo(\"DynamicProxyGenAssembly2");
+    }
+
+    public class when_trying_to_override_internal_method_on_type
+    {
+        private static TypeWithInternalMethod fake;
+        private static Exception exception;
+
+        private Establish context = () => fake = A.Fake<TypeWithInternalMethod>();
+
+        private Because of = () => exception = Catch.Exception(() => A.CallTo(() => fake.InternalMethod()).Returns(17));
+
+        private It should_throw_an_exception_with_a_message_complaining_about_accessibility =
+            () =>
+                exception.Should().BeAnExceptionOfType<FakeConfigurationException>()
+                    .And.Message.Should().Contain("not accessible to DynamicProxyGenAssembly2");
     }
 }
