250889fa8743462f7f7e326de803d0de81a24de7
Blair Conrad <blair@blairconrad.com>
2014-10-17 06:09:28 -0400

    #359 - removing reference to "shorter syntax" and "Go" in example

--- Source/FakeItEasy.Examples.VB/RaisingEvents.vb
+++ Source/FakeItEasy.Examples.VB/RaisingEvents.vb
@@ -4,10 +4,10 @@ Public Class RaisingEvents
     Public Sub Raising_an_event()
         Dim widget = A.Fake(Of IWidget)()
 
-        ' Raise EventHandler(Of T) with the shorter syntax and omitting Go
+        ' Raise EventHandler(Of T)
         AddHandler widget.WidgetBroke, Raise.With(New WidgetEventArgs("foo"))
 
-        ' Raise EventHandler with the shorter syntax and omitting Go
+        ' Raise EventHandler
         AddHandler widget.WidgetRunning, Raise.With(New EventArgs())
     End Sub
 End Class
