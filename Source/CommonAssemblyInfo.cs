5a9214cafa3a6f0d610b23a83dc837a7acaaa1e4
Blair Conrad <blair@blairconrad.com>
2015-01-05 20:20:36 -0500

    setting version to 2.0.0-beta004

--- Source/CommonAssemblyInfo.cs
+++ Source/CommonAssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.0-beta004")]
+[assembly: AssemblyFileVersion("2.0.0-beta004")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta004")]
 
 [assembly: AssemblyCompany("Patrik Hägne")]
 [assembly: AssemblyProduct("FakeItEasy")]
