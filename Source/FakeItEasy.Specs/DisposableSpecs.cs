005807b806674c2f1992d324f83955b7be75e822
Blair Conrad <blair@blairconrad.com>
2015-01-24 20:16:35 -0500

    added failing spec for Disposable fake

---
+++ Source/FakeItEasy.Specs/DisposableSpecs.cs
@@ -0,0 +1,55 @@
+﻿namespace FakeItEasy.Specs
+{
+    using System;
+    using FluentAssertions;
+    using Machine.Specifications;
+
+    public class when_faking_a_disposable_class
+        : EventRaisingSpecs
+    {
+        static IDisposable fake;
+        private static Exception exception;
+
+        Establish context = () =>
+        {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionHandler;
+
+            fake = A.Fake<SomeDisposable>();
+        };
+
+        Because of = () =>
+        {
+            fake = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        };
+
+        It should_not_throw_when_finalized = () => exception.Should().BeNull();
+
+        Cleanup after = () =>
+        {
+            AppDomain.CurrentDomain.UnhandledException -= UnhandledExceptionHandler;
+        };
+
+        private static void UnhandledExceptionHandler(object sender, UnhandledExceptionEventArgs e)
+        {
+            exception = (Exception)e.ExceptionObject;
+        }
+
+        public abstract class SomeDisposable : IDisposable
+        {
+            ~SomeDisposable()
+            {
+                this.Dispose(false);
+            }
+
+            public void Dispose()
+            {
+                this.Dispose(true);
+                GC.SuppressFinalize(this);
+            }
+
+            protected abstract void Dispose(bool shouldCleanupManaged);
+        }
+    }
+}
---
+++ Source/FakeItEasy.Specs/GlobalSuppressions.cs
@@ -0,0 +1,17 @@
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "Microsoft.Reliability",
+    "CA2001:AvoidCallingProblematicMethods",
+    MessageId = "System.GC.Collect",
+    Scope = "member",
+    Target = "FakeItEasy.Specs.when_faking_a_disposable_class.#.ctor()",
+    Justification = "Required for testing.")]
+
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Code Analysis results, point to "Suppress Message", and click 
+// "In Suppression File".
+// You do not need to add suppressions to this file manually.
