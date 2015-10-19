0bf2c9761105047ed2ce968f632fcc28ade9a036
Blair Conrad <blair@blairconrad.com>
2015-10-19 18:51:36 -0400

    removed duplication in FakeConfiguratorSpecs

--- Source/FakeItEasy.Specs/FakeConfiguratorSpecs.cs
+++ Source/FakeItEasy.Specs/FakeConfiguratorSpecs.cs
@@ -15,6 +15,9 @@
 
             "then it should be configured"
                 .x(() => fake.ID.Should().BeGreaterThan(0));
+
+            "and it should be configured before its constructor is run"
+                .x(() => fake.Timestamp.Should().Be(DomainEventFakeConfigurator.ConfiguredTimestamp));
         }
 
         [Scenario]
@@ -27,21 +30,12 @@
             "then it should be configured by the one with higher priority"
                 .x(() => fake.ID.Should().Be(-99));
         }
-
-        [Scenario]
-        public static void DuringConstruction(
-            RobotRunsAmokEvent fake)
-        {
-            "when creating a fake that has a matching configurator"
-                .x(() => fake = A.Fake<RobotRunsAmokEvent>());
-
-            "then it should be configured before its constructor is run"
-                .x(() => fake.Timestamp.Should().Be(RobotRunsAmokEventFakeConfigurator.ConfiguredTimestamp));
-        }
     }
 
     public class DomainEventFakeConfigurator : IFakeConfigurator
     {
+        public static readonly DateTime ConfiguredTimestamp = new DateTime(1997, 8, 29, 2, 14, 03);
+
         private int nextID = 1;
 
         public int Priority
@@ -56,19 +50,18 @@
 
         public void ConfigureFake(object fakeObject)
         {
-            ((DomainEvent)fakeObject).ID = this.nextID++;
+            var domainEvent = (DomainEvent)fakeObject;
+            domainEvent.ID = this.nextID++;
+            A.CallTo(() => domainEvent.CalculateTimestamp()).Returns(ConfiguredTimestamp);
         }
     }
 
     public class RobotRunsAmokEventFakeConfigurator : FakeConfigurator<RobotRunsAmokEvent>
     {
-        public static readonly DateTime ConfiguredTimestamp = new DateTime(1997, 8, 29, 2, 14, 03);
-
         protected override void ConfigureFake(RobotRunsAmokEvent fakeObject)
         {
             if (fakeObject != null)
             {
-                A.CallTo(() => fakeObject.CalculateTimestamp()).Returns(ConfiguredTimestamp);
                 fakeObject.ID = -99;
             }
         }
