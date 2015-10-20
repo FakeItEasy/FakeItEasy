ddd5b957b2435265e5c02f8db084f7065089f684
Blair Conrad <blair@blairconrad.com>
2015-10-20 06:08:45 -0400

    extracting "use fake outside scope" scenario

--- Source/FakeItEasy.Specs/FakeScopeSpecs.cs
+++ Source/FakeItEasy.Specs/FakeScopeSpecs.cs
@@ -14,17 +14,10 @@
             MakesVirtualCallInConstructor fake)
         {
             "given an object container"
-                .x(() =>
-                {
-                    fakeObjectContainer = A.Fake<IFakeObjectContainer>();
-                    A.CallTo(() => fakeObjectContainer.ConfigureFake(A<Type>._, A<object>._))
-                        .Invokes(
-                            (Type t, object options) =>
-                                A.CallTo(options).WithReturnType<string>().Returns("configured value in fake scope"));
-                });
+                .x(() => fakeObjectContainer = CreateFakeObjectContainer("configured value in fake scope"));
 
             "and a fake scope using that container"
-                .x(() => scope = Fake.CreateScope(fakeObjectContainer));
+                .x(context => scope = Fake.CreateScope(fakeObjectContainer).Using(context));
 
             "when a fake is created inside the scope"
                 .x(() => fake = A.Fake<MakesVirtualCallInConstructor>());
@@ -38,13 +31,41 @@
 
             "and the object container's configuration should be used after the constructor"
                 .x(() => fake.VirtualMethod("call after constructor").Should().Be("configured value in fake scope"));
+        }
 
-            "and the object container's configuration should not be used outside the scope"
+        [Scenario]
+        public static void UsingFakeOutsideOfScope(
+            IFakeObjectContainer fakeObjectContainer,
+            MakesVirtualCallInConstructor fake,
+            string virtualMethodValueOutsideOfScope)
+        {
+            "given an object container"
+                .x(() => fakeObjectContainer = CreateFakeObjectContainer("configured value in fake scope"));
+
+            "and a fake created within a scope using that container"
                 .x(() =>
                 {
-                    scope.Dispose();
-                    fake.VirtualMethod("call outside scope").Should().Be(string.Empty);
+                    using (Fake.CreateScope(fakeObjectContainer))
+                    {
+                        fake = A.Fake<MakesVirtualCallInConstructor>();
+                    }
                 });
+
+            "when the fake is accessed outside the scope"
+                .x(() => virtualMethodValueOutsideOfScope = fake.VirtualMethod("call outside scope"));
+
+            "then the object container's configuration should not be used"
+                .x(() => virtualMethodValueOutsideOfScope.Should().Be(string.Empty));
+        }
+
+        private static IFakeObjectContainer CreateFakeObjectContainer(string stringMethodValue)
+        {
+            var fakeObjectContainer = A.Fake<IFakeObjectContainer>();
+            A.CallTo(() => fakeObjectContainer.ConfigureFake(A<Type>._, A<object>._))
+                .Invokes(
+                    (Type t, object options) =>
+                        A.CallTo(options).WithReturnType<string>().Returns(stringMethodValue));
+            return fakeObjectContainer;
         }
     }
 }
