06aec3cb59f8d7b76662b5255dd2257f1252ca6c
Blair Conrad <blair@blairconrad.com>
2016-01-16 07:56:28 -0500

    throwing when FakeOptionsBuilder<T>.BuildOptions passed wrong type

---
+++ Source/FakeItEasy.Tests/FakeOptionsBuilderTests.cs
@@ -0,0 +1,32 @@
+﻿namespace FakeItEasy.Tests
+{
+    using System;
+    using FakeItEasy.Creation;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using TestHelpers;
+
+    public class FakeOptionsBuilderTests
+    {
+        [Test]
+        public void BuildOptions_should_throw_when_passed_wrong_type()
+        {
+            // Arrange
+            var target = new FakeOptionsBuilderTestsOptionsBuilder();
+
+            // Act
+            var exception = Record.Exception(() => target.BuildOptions(typeof(string), A.Fake<IFakeOptions>()));
+
+            // Assert
+            exception.Should().BeAnExceptionOfType<InvalidOperationException>()
+                .WithMessage("Specified type 'System.String' is not valid. Only 'FakeItEasy.Tests.FakeOptionsBuilderTests' is allowed.");
+        }
+
+        private class FakeOptionsBuilderTestsOptionsBuilder : FakeOptionsBuilder<FakeOptionsBuilderTests>
+        {
+            protected override void BuildOptions(IFakeOptions<FakeOptionsBuilderTests> options)
+            {
+            }
+        }
+    }
+}
