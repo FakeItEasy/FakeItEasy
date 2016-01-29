50303b1893940129ecfad5084f9a8eb97898c8e2
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2016-01-29 21:26:50 +0100

    Add failing tests demonstrating the problem
    For Task<T> and Lazy<T>, if a dummy T cannot be created, the task result
    (or the lazy value) should be default(T).
    
    Rename AsyncAwaitSpecs to TaskSpecs

--- Source/FakeItEasy.Specs/LazySpecs.cs
+++ Source/FakeItEasy.Specs/LazySpecs.cs
@@ -11,6 +11,8 @@ namespace FakeItEasy.Specs
         public interface ILazyFactory
         {
             Lazy<IFoo> Create();
+
+            Lazy<Bar> CreateBar();
         }
 
         [SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Justification = "Required for testing.")]
@@ -36,6 +38,24 @@ namespace FakeItEasy.Specs
                 .x(() => lazy.Value.Should().Be(FooFactory.Instance));
         }
 
+        [Scenario]
+        public static void LazyReturnValueOfNonFakeableType(
+            ILazyFactory fake,
+            Lazy<Bar> lazy)
+        {
+            "Given a fake"
+                .x(() => fake = A.Fake<ILazyFactory>());
+
+            "When calling a method that returns a lazy"
+                .x(() => lazy = fake.CreateBar());
+
+            "Then it should return a lazy"
+                .x(() => lazy.Should().NotBeNull());
+
+            "And the value of the lazy should be null"
+                .x(() => lazy.Value.Should().BeNull());
+        }
+
         public class FooFactory : DummyFactory<IFoo>, IFoo
         {
             private static IFoo instance = new FooFactory();
@@ -48,6 +68,13 @@ namespace FakeItEasy.Specs
             protected override IFoo Create()
             {
                 return instance;
+            }
+        }
+
+        public class Bar
+        {
+            internal Bar()
+            {
             }
         }
     }
---
+++ Source/FakeItEasy.Specs/TaskSpecs.cs
@@ -0,0 +1,169 @@
+﻿namespace FakeItEasy.Specs
+{
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Xbehave;
+
+    public static class TaskSpecs
+    {
+        public interface IFooAsyncAwaitService
+        {
+            Task CommandAsync();
+
+            Task<int> QueryAsync();
+
+            Task<Bar> GetBarAsync();
+        }
+
+        [Scenario]
+        public static void DefinedMethodWithReturnValue(
+            FooAsyncAwait foo,
+            Task<int> task)
+        {
+            "establish"
+                .x(() =>
+                    {
+                        var service = A.Fake<IFooAsyncAwaitService>();
+                        A.CallTo(() => service.QueryAsync()).Returns(Task.FromResult(9));
+                        foo = new FooAsyncAwait(service);
+                    });
+
+            "when calling defined method with return value"
+                .x(() => task = foo.QueryAsync());
+
+            "it should return"
+                .x(() => task.IsCompleted.Should().BeTrue());
+
+            "the configured value"
+                .x(() => task.Result.Should().Be(9));
+        }
+
+        [Scenario]
+        public static void DefinedVoidMethod(
+            FooAsyncAwait foo,
+            Task task)
+        {
+            "establish"
+                .x(() =>
+                    {
+                        var service = A.Fake<IFooAsyncAwaitService>();
+                        A.CallTo(() => service.CommandAsync()).Returns(Task.FromResult<object>(null));
+                        foo = new FooAsyncAwait(service);
+                    });
+
+            "when calling defined void method"
+                .x(() => task = foo.CommandAsync());
+
+            "it should return"
+                .x(() => task.IsCompleted.Should().BeTrue());
+        }
+
+        [Scenario]
+        public static void DefinedMethodWithReturnValueOfNonFakeableType(
+            FooAsyncAwait foo,
+            Bar bar,
+            Task<Bar> task)
+        {
+            "establish"
+                .x(() =>
+                {
+                        bar = new Bar();
+                        var service = A.Fake<IFooAsyncAwaitService>();
+                        A.CallTo(() => service.GetBarAsync()).Returns(Task.FromResult(bar));
+                        foo = new FooAsyncAwait(service);
+                    });
+
+            "when calling defined method with return value"
+                .x(() => task = foo.GetBarAsync());
+
+            "it should return"
+                .x(() => task.IsCompleted.Should().BeTrue());
+
+            "the configured value"
+                .x(() => task.Result.Should().Be(bar));
+        }
+
+        [Scenario]
+        public static void UndefinedMethodWithReturnValue(
+            FooAsyncAwait foo,
+            Task<int> task)
+        {
+            "establish"
+                .x(() => foo = new FooAsyncAwait(A.Fake<IFooAsyncAwaitService>()));
+
+            "when calling undefined method with return value"
+                .x(() => task = foo.QueryAsync());
+
+            "it should return"
+                .x(() => task.IsCompleted.Should().BeTrue());
+
+            "a dummy value"
+                .x(() => task.Result.Should().Be(0));
+        }
+
+        [Scenario]
+        public static void UndefinedVoidMethod(
+            FooAsyncAwait foo,
+            Task task)
+        {
+            "establish"
+                .x(() => foo = new FooAsyncAwait(A.Fake<IFooAsyncAwaitService>()));
+
+            "when calling undefined void method"
+                .x(() => task = foo.CommandAsync());
+
+            "it should return"
+                .x(() => task.IsCompleted.Should().BeTrue());
+        }
+
+        [Scenario]
+        public static void UndefinedMethodReturnValueOfNonFakeableType(
+            FooAsyncAwait foo,
+            Task<Bar> task)
+        {
+            "establish"
+                .x(() => foo = new FooAsyncAwait(A.Fake<IFooAsyncAwaitService>()));
+
+            "when calling undefined method with return value"
+                .x(() => task = foo.GetBarAsync());
+
+            "it should return"
+                .x(() => task.IsCompleted.Should().BeTrue());
+
+            "and the task result should be null"
+                .x(() => task.Result.Should().BeNull());
+        }
+
+        public class Bar
+        {
+            internal Bar()
+            {
+            }
+        }
+
+        public class FooAsyncAwait
+        {
+            private readonly IFooAsyncAwaitService service;
+
+            public FooAsyncAwait(IFooAsyncAwaitService service)
+            {
+                this.service = service;
+            }
+
+            public async Task CommandAsync()
+            {
+                await this.service.CommandAsync();
+            }
+
+            public async Task<int> QueryAsync()
+            {
+                return await this.service.QueryAsync();
+            }
+
+            public async Task<Bar> GetBarAsync()
+            {
+                return await this.service.GetBarAsync();
+            }
+        }
+    }
+}
