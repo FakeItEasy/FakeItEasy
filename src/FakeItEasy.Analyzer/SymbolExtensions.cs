159d1987630415217c94f5ae24566a9f71d03165
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2016-04-04 23:17:22 +0200

    Add analyzer to warn about incorrect usage
    There's only one analyzer so far; it checks for unused call
    specifications, i.e. calls to A.CallTo(...) that are not used.
    At this point the analysis doesn't try to be very smart, it just check if
    the call to A.CallTo(...) is the only thing in the statement.

---
+++ src/FakeItEasy.Analyzer/SymbolExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace FakeItEasy.Analyzer
+{
+    using System;
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+
+    internal static class SymbolExtensions
+    {
+        /// <summary>
+        /// Returns the full name of a type, including its namespace and containing type.
+        /// </summary>
+        /// <param name="type">The type for which to get the full name.</param>
+        /// <returns>The full name of the type.</returns>
+        public static string GetFullName(this INamedTypeSymbol type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            var nameParts = new Stack<string>();
+            nameParts.Push(type.Name);
+            var containingType = type.ContainingType;
+            while (containingType != null)
+            {
+                nameParts.Push(containingType.Name);
+                containingType = containingType.ContainingType;
+            }
+
+            var containingNamespace = type.ContainingNamespace;
+            while (!string.IsNullOrEmpty(containingNamespace?.Name))
+            {
+                nameParts.Push(containingNamespace.Name);
+                containingNamespace = containingNamespace.ContainingNamespace;
+            }
+
+            return string.Join(".", nameParts);
+        }
+    }
+}
---
+++ tests/FakeItEasy.Analyzer.Tests/Helpers/DiagnosticResult.cs
@@ -0,0 +1,93 @@
+﻿namespace FakeItEasy.Analyzer.Tests.Helpers
+{
+    using System;
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// Location where the diagnostic appears, as determined by path, line number, and column number.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes", Justification = "Not necessary")]
+    internal struct DiagnosticResultLocation
+    {
+        public DiagnosticResultLocation(string path, int line, int column)
+        {
+            if (line < 0 && column < 0)
+            {
+                throw new ArgumentException("At least one of line and column must be > 0");
+            }
+
+            if (line < -1 || column < -1)
+            {
+                throw new ArgumentException("Both line and column must be >= -1");
+            }
+
+            this.Path = path;
+            this.Line = line;
+            this.Column = column;
+        }
+
+        public string Path { get; set; }
+
+        public int Line { get; set; }
+
+        public int Column { get; set; }
+    }
+
+    /// <summary>
+    /// Struct that stores information about a Diagnostic appearing in a source.
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1815:OverrideEqualsAndOperatorEqualsOnValueTypes", Justification = "Not necessary")]
+    internal struct DiagnosticResult
+    {
+        private DiagnosticResultLocation[] locations;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1819:PropertiesShouldNotReturnArrays", Justification = "It's not intended to be a public API, so it doesn't matter")]
+        public DiagnosticResultLocation[] Locations
+        {
+            get
+            {
+                if (this.locations == null)
+                {
+                    this.locations = new DiagnosticResultLocation[] { };
+                }
+
+                return this.locations;
+            }
+
+            set
+            {
+                this.locations = value;
+            }
+        }
+
+        public DiagnosticSeverity Severity { get; set; }
+
+        public string Id { get; set; }
+
+        public string Message { get; set; }
+
+        public string Path
+        {
+            get
+            {
+                return this.Locations.Length > 0 ? this.Locations[0].Path : string.Empty;
+            }
+        }
+
+        public int Line
+        {
+            get
+            {
+                return this.Locations.Length > 0 ? this.Locations[0].Line : -1;
+            }
+        }
+
+        public int Column
+        {
+            get
+            {
+                return this.Locations.Length > 0 ? this.Locations[0].Column : -1;
+            }
+        }
+    }
+}
