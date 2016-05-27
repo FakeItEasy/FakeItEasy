3b95f98ea2a9837cf86835eed8807a96dda8bd18
Blair Conrad <blair@blairconrad.com>
2016-05-27 06:27:23 -0400

    Make pre_release task

--- rakefile.rb
+++ rakefile.rb
@@ -38,31 +38,42 @@ specs = "tests/FakeItEasy.Specs/bin/Release/FakeItEasy.Specs.dll"
 approval_tests = "tests/FakeItEasy.Tests.Approval/bin/Release/FakeItEasy.Tests.Approval.dll"
 
 repo = 'FakeItEasy/FakeItEasy'
+ci_server_url = 'http://teamcity.codebetter.com/admin/editBuildParams.html?id=buildType:bt929'
 release_issue_labels = ['P2', 'build', 'documentation']
-release_issue_body = <<-eos
-**Ready** when all other issues on this milestone are **Done** and closed.
+release_issue_common_steps = <<-eos
+**in-progress** when all other issues on this milestone are closed.
 
 - [ ] run code analysis in VS in *Release* mode and address violations (send a regular PR which must be merged before continuing)
-- [ ] if necessary, change `VERSION_SUFFIX` on [CI Server](http://teamcity.codebetter.com/admin/editBuildParams.html?id=buildType:bt929)
+- [ ] if necessary, change `VERSION_SUFFIX` on the [CI Server](#{ci_server_url})
       to appropriate "-beta123" or "" (for non-betas) value and initiate a build
 - [ ] check build
--  edit draft release in [GitHub UI](https://github.com/FakeItEasy/FakeItEasy/releases):
+- edit draft release in [GitHub UI](https://github.com/FakeItEasy/FakeItEasy/releases):
     - [ ] complete release notes, mentioning non-owner contributors, if any (move release notes forward from any pre-releases to the current release)
-    - [ ] attach nupkg(s) - main package and/or analyzer, whichever have new content
+    - [ ] attach main nupkg and/or analyzer nupkg, whichever have content to release
     - [ ] publish the release
-- [ ] push NuGet package
+- [ ] push nupkg(s) to NuGet
 - [ ] de-list pre-release or superseded buggy NuGet packages if present
 - [ ] update website with contributors list (if in place)
 - [ ] tweet, mentioning contributors and post link as comment here for easy retweeting ;-)
 - [ ] post tweet in [Gitter](https://gitter.im/FakeItEasy/FakeItEasy)
-- [ ] post links to the NuGet and GitHub release in each issue in this milestone, with thanks to contributors
+- [ ] post links to the GitHub Release in each issue in this milestone, with thanks to contributors
+eos
+
+next_pre_release_steps = <<-eos
+- if there's to be a new pre-release issue
+    - [ ] run `rake pre_release[version_suffix]` to
+        - create a new draft GitHub Release
+        - new issue (like this one) for the next release, adding it to the current milestone
+    - [ ] change `VERSION_SUFFIX` on the [CI Server](#{ci_server_url})
+eos
+
+next_version_steps = <<-eos
 - [ ] run `rake next_version[new_version]` to
     - create a pull request that changes the version in CommonAssemblyInfo.cs to the expected version (of form _xx.yy.zz_)
+    - create a new draft GitHub Release
     - create a new milestone for the next release
     - create a new issue (like this one) for the next release, adding it to the new milestone
-    - create a new draft GitHub Release
 - [ ] close this milestone
-
 eos
 
 release_body = <<-eos
@@ -147,35 +158,49 @@ task :next_version, :new_version do |asm, args|
   )
   puts "Created pull request \##{pull_request.number} '#{pull_request.title}'."
 
-  release_description = new_version + ' release'
-
   puts "Creating milestone '#{new_version}'..."
   milestone = client.create_milestone(
     repo,
     new_version,
-    :description => release_description
+    :description => new_version + ' release'
     )
   puts "Created milestone '#{new_version}'."
 
-  puts "Creating issue '#{release_description}'..."
-  issue = client.create_issue(
-    repo,
-    release_description,
-    release_issue_body,
-    :labels => release_issue_labels,
-    :milestone => milestone.number
-    )
-  puts "Created issue \##{issue.number} '#{release_description}'."
+  create_release(
+                 client,
+                 repo,
+                 milestone,
+                 new_version,
+                 release_body,
+                 release_issue_common_steps + next_version_steps,
+                 release_issue_labels)
+end
 
-  puts "Creating release '#{new_version}'..."
-  client.create_release(
-    repo,
-    new_version,
-    :name => new_version,
-    :draft => true,
-    :body => release_body
-    )
-  puts "Created release '#{new_version}'."
+desc "Create GitHub Release and release checklist issue for pre-release build"
+task :pre_release, :version_suffix do |asm, args|
+  version_suffix = args.version_suffix or
+    fail 'ERROR: A version suffix is required, e.g.: rake pre_release[beta001]'
+
+  require 'octokit'
+
+  ssl_cert_file = get_temp_ssl_cert_file(ssl_cert_file_url)
+
+  client = Octokit::Client.new(:netrc => true)
+
+  milestone = client
+    .list_milestones(repo, :state => 'open')
+    .select { |m| m.title == version }
+    .first or
+    raise "ERROR: can't find existing milestone #{version}"
+
+  create_release(
+                 client,
+                 repo,
+                 milestone,
+                 "#{version}-#{version_suffix}",
+                 release_body,
+                 release_issue_common_steps + next_pre_release_steps,
+                 release_issue_labels)
 end
 
 desc "Update assembly info"
@@ -249,6 +274,29 @@ exec :pack => [:build, output] do |cmd|
   cmd.parameters "pack #{analyzer_nuspec} -Version #{version}#{version_suffix} -OutputDirectory #{output}"
 end
 
+def create_release(client, repo, milestone, release_version, release_body, release_issue_body, release_issue_labels)
+  release_description = release_version + ' release'
+  puts "Creating issue '#{release_description}'..."
+  issue = client.create_issue(
+    repo,
+    release_description,
+    release_issue_body,
+    :labels => release_issue_labels,
+    :milestone => milestone.number
+    )
+  puts "Created issue \##{issue.number} '#{release_description}'."
+
+  puts "Creating release '#{release_version}'..."
+  client.create_release(
+    repo,
+    release_version,
+    :name => release_version,
+    :draft => true,
+    :body => release_body
+    )
+  puts "Created release '#{release_version}'."
+end
+
 def print_vars(variables)
 
   scalars = []
@@ -274,7 +322,13 @@ def print_vars(variables)
   }
 
   puts
-  vectors.select { |name, value| !['release_body', 'release_issue_body', 'release_issue_labels'].include? name }.each { |name, value|
+  vectors.select { |name, value| ![
+                                   'release_body',
+                                   'release_issue_common_steps',
+                                   'next_version_steps',
+                                   'next_pre_release_steps',
+                                   'release_issue_labels'
+                                  ].include? name }.each { |name, value|
     puts "#{name}:"
     puts value.map {|v| "  " + v }
     puts ""
