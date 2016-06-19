c0334e5cb3433b8d016b0ad83857cf18be15bd9d
Adam Ralph <adam@adamralph.com>
2016-06-19 23:21:21 +0100

    remove docs on internal IFakeCreatorFacade and DefaultFakeCreatorFacade

--- src/FakeItEasy/Creation/DefaultFakeCreatorFacade.cs
+++ src/FakeItEasy/Creation/DefaultFakeCreatorFacade.cs
@@ -2,30 +2,16 @@ namespace FakeItEasy.Creation
 {
     using System;
 
-    /// <summary>
-    /// Default implementation of the IFakeCreator-interface.
-    /// </summary>
     internal class DefaultFakeCreatorFacade
         : IFakeCreatorFacade
     {
         private readonly IFakeAndDummyManager fakeAndDummyManager;
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DefaultFakeCreatorFacade"/> class.
-        /// </summary>
-        /// <param name="fakeAndDummyManager">The fake and dummy manager.</param>
         public DefaultFakeCreatorFacade(IFakeAndDummyManager fakeAndDummyManager)
         {
             this.fakeAndDummyManager = fakeAndDummyManager;
         }
 
-        /// <summary>
-        /// Creates a fake object of the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type of fake to create.</typeparam>
-        /// <param name="optionsBuilder">Action that builds options for the created fake object.</param>
-        /// <returns>The created fake object.</returns>
-        /// <exception cref="FakeCreationException">Was unable to generate the fake in the current configuration.</exception>
         public T CreateFake<T>(Action<IFakeOptions<T>> optionsBuilder)
         {
             Guard.AgainstNull(optionsBuilder, "optionsBuilder");
@@ -41,14 +27,6 @@ namespace FakeItEasy.Creation
             return this.fakeAndDummyManager.CreateFake(typeOfFake, optionsBuilder);
         }
 
-        /// <summary>
-        /// Creates a dummy object, this can be a fake object or an object resolved
-        /// from the current IFakeObjectContainer.
-        /// </summary>
-        /// <typeparam name="T">The type of dummy to create.</typeparam>
-        /// <returns>The created dummy.</returns>
-        /// <exception cref="FakeCreationException">Was unable to generate the fake in the current configuration and
-        /// no dummy was registered in the container for the specified type..</exception>
         public T CreateDummy<T>()
         {
             return (T)this.fakeAndDummyManager.CreateDummy(typeof(T));
--- src/FakeItEasy/Creation/IFakeCreatorFacade.cs
+++ src/FakeItEasy/Creation/IFakeCreatorFacade.cs
@@ -2,47 +2,14 @@ namespace FakeItEasy.Creation
 {
     using System;
 
-    /// <summary>
-    /// A facade used by the public API for testability.
-    /// </summary>
     internal interface IFakeCreatorFacade
     {
-        /// <summary>
-        /// Creates a fake object of the specified type.
-        /// </summary>
-        /// <typeparam name="T">The type of fake to create.</typeparam>
-        /// <param name="optionsBuilder">Action that builds options for the created fake object.</param>
-        /// <returns>The created fake object.</returns>
-        /// <exception cref="FakeItEasy.Core.FakeCreationException">Was unable to generate the fake in the current configuration.</exception>
         T CreateFake<T>(Action<IFakeOptions<T>> optionsBuilder);
 
-        /// <summary>
-        /// Creates a fake object of the specified type.
-        /// </summary>
-        /// <param name="typeOfFake">The type of fake to create.</param>
-        /// <param name="optionsBuilder">Action that builds options for the created fake object.</param>
-        /// <returns>The created fake object.</returns>
-        /// <exception cref="FakeItEasy.Core.FakeCreationException">Was unable to generate the fake in the current configuration.</exception>
         object CreateFake(Type typeOfFake, Action<IFakeOptions> optionsBuilder);
 
-        /// <summary>
-        /// Creates a dummy object, this can be a fake object or an object resolved
-        /// from a dummy factory.
-        /// </summary>
-        /// <typeparam name="T">The type of dummy to create.</typeparam>
-        /// <returns>The created dummy.</returns>
-        /// <exception cref="FakeItEasy.Core.FakeCreationException">Was unable to generate the fake in the current configuration and
-        /// no dummy was registered in the container for the specified type.</exception>
         T CreateDummy<T>();
 
-        /// <summary>
-        /// Creates a dummy object, this can be a fake object or an object resolved
-        /// from a dummy factory.
-        /// </summary>
-        /// <param name="typeOfDummy">The type of dummy to create.</param>
-        /// <returns>The created dummy.</returns>
-        /// <exception cref="FakeItEasy.Core.FakeCreationException">Was unable to generate the fake in the current configuration and
-        /// no dummy was registered in the container for the specified type.</exception>
         object CreateDummy(Type typeOfDummy);
     }
 }
