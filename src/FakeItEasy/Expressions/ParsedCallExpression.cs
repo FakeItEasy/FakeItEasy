686f8d1c2eaf021e2cb3c9ebb8ca34f398188e7a
Blair Conrad <blair@blairconrad.com>
2016-06-23 09:50:32 -0400

    Evaluate call target expression at most once

--- src/FakeItEasy/Expressions/ParsedCallExpression.cs
+++ src/FakeItEasy/Expressions/ParsedCallExpression.cs
@@ -1,5 +1,6 @@
 namespace FakeItEasy.Expressions
 {
+    using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
@@ -7,30 +8,27 @@ namespace FakeItEasy.Expressions
 
     internal class ParsedCallExpression
     {
+        private readonly Lazy<object> callTarget;
+
         public ParsedCallExpression(
             MethodInfo calledMethod,
             Expression callTargetExpression,
             IEnumerable<ParsedArgumentExpression> argumentsExpressions)
         {
             this.CalledMethod = calledMethod;
-            this.CallTargetExpression = callTargetExpression;
             this.ArgumentsExpressions = argumentsExpressions;
+            this.callTarget = new Lazy<object>(() => callTargetExpression?.Evaluate());
         }
 
         public MethodInfo CalledMethod { get; private set; }
 
         public IEnumerable<ParsedArgumentExpression> ArgumentsExpressions { get; private set; }
 
-        public object CallTarget
-        {
-            get { return this.CallTargetExpression != null ? this.CallTargetExpression.Evaluate() : null; }
-        }
+        public object CallTarget => this.callTarget.Value;
 
         public IEnumerable<object> Arguments
         {
             get { return this.ArgumentsExpressions.Select(x => x.Value); }
         }
-
-        private Expression CallTargetExpression { get; set; }
     }
 }
