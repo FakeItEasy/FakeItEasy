dd66178dd9b632d3600b84f6df0e8014a1f7521c
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2016-12-14 00:13:17 +0100

    Add failing tests demonstrating the issue

---
+++ tests/FakeItEasy.IntegrationTests.VB/RefArgumentConstraintTests.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Runtime.InteropServices
+Imports FluentAssertions
+Imports Xunit
+
+Public Class RefArgumentConstraintTests
+
+    Public Interface IFoo
+        Function Bar(ByRef x As String) As Integer
+        Function Baz(<Out> ByRef x As String) As Integer
+    End Interface
+
+    <Fact>
+    Public Sub Constraint_on_ref_argument_is_honored()
+        ' Arrange
+        Dim foo = A.Fake(Of IFoo)
+        A.CallTo(Function() foo.Bar(A(Of String).That.StartsWith("h"))).Returns(42)
+
+        ' Act
+        Dim result = foo.Bar("hello")
+
+        ' Assert
+        result.Should().Be(42)
+    End Sub
+
+    <Fact>
+    Public Sub Constraint_on_out_argument_is_ignored()
+        ' Arrange
+        Dim foo = A.Fake(Of IFoo)
+        A.CallTo(Function() foo.Baz(A(Of String).That.StartsWith("h"))).Returns(42)
+
+        ' Act
+        Dim result = foo.Baz("blah")
+
+        ' Assert
+        result.Should().Be(42)
+    End Sub
+
+End Class
