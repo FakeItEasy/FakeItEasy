9dc978d5a2baaebd783c6ae497b7a45e0ea21833
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2017-02-17 20:13:52 +0100

    Update existing specs to GWT style

--- tests/FakeItEasy.Specs/StrictFakeSpecs.cs
+++ tests/FakeItEasy.Specs/StrictFakeSpecs.cs
@@ -24,25 +24,26 @@ namespace FakeItEasy.Specs
             IMyInterface fake,
             Exception exception)
         {
-            "establish"
+            "Given a strict fake"
+                .x(() => fake = A.Fake<IMyInterface>(o => o.Strict()));
+
+            "And a call configured to invoke a delegate"
+                .x(() => A.CallTo(() => fake.DoIt(Guid.Empty)).Invokes(() => { }));
+
+            "When I call the configured method twice with a matching argument"
                 .x(() =>
                     {
-                        fake = A.Fake<IMyInterface>(o => o.Strict());
-                        ////fake = A.Fake<IMyInterface>();
-
-                        A.CallTo(() => fake.DoIt(Guid.Empty)).Invokes(() => { });
-
                         fake.DoIt(Guid.Empty);
                         fake.DoIt(Guid.Empty);
                     });
 
-            "when asserting must have happened when did not happen"
+            "And I assert that the call happened exactly once"
                 .x(() => exception = Record.Exception(() => A.CallTo(() => fake.DoIt(Guid.Empty)).MustHaveHappened(Repeated.Exactly.Once)));
 
-            "it should throw an expectation exception"
+            "Then it should throw an expectation exception"
                 .x(() => exception.Should().BeAnExceptionOfType<ExpectationException>());
 
-            "it should have an exception message containing the name of the method"
+            "And it should have an exception message containing the name of the method"
                 .x(() => exception.Message.Should().Contain("DoIt"));
         }
 
