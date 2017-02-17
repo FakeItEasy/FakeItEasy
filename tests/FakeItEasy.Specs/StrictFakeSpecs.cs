e5970ddf520d6a7e7a34868a140ee57826bf3bb2
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2017-02-17 20:06:25 +0100

    Add failing specs for strict fake object members

--- tests/FakeItEasy.Specs/StrictFakeSpecs.cs
+++ tests/FakeItEasy.Specs/StrictFakeSpecs.cs
@@ -6,13 +6,19 @@ namespace FakeItEasy.Specs
     using Xbehave;
     using Xunit;
 
-    public interface IMyInterface
-    {
-        void DoIt(Guid id);
-    }
-
     public static class StrictFakeSpecs
     {
+        public interface IMyInterface
+        {
+            void DoIt(Guid id);
+
+            int GetIt(IFoo foo);
+        }
+
+        public interface IFoo
+        {
+        }
+
         [Scenario]
         public static void RepeatedAssertion(
             IMyInterface fake,
@@ -38,6 +44,78 @@ namespace FakeItEasy.Specs
 
             "it should have an exception message containing the name of the method"
                 .x(() => exception.Message.Should().Contain("DoIt"));
+        }
+
+        [Scenario]
+        public static void CallDescriptionWithStrictFakeParameter(
+            IMyInterface fake,
+            IFoo parameter,
+            Exception ex)
+        {
+            "Given a fake"
+                .x(() => fake = A.Fake<IMyInterface>());
+
+            "And a strict fake parameter"
+                .x(() => parameter = A.Fake<IFoo>(o => o.Strict()));
+
+            "When I assert that the fake has been called with the fake parameter"
+                .x(() => ex = Record.Exception(() => A.CallTo(() => fake.GetIt(parameter)).MustHaveHappened()));
+
+            "Then it throws an expectation exception"
+                .x(() => ex.Should().BeAnExceptionOfType<ExpectationException>());
+
+            "And the exception message should say that the call didn't happen"
+                .x(() => ex.Message
+                        .Should().Contain("Assertion failed for the following call:")
+                        .And.Contain("FakeItEasy.Specs.StrictFakeSpecs+IMyInterface.GetIt(Faked FakeItEasy.Specs.StrictFakeSpecs+IFoo)"));
+        }
+
+        [Scenario]
+        public static void CallMatchingSuccessWithStrictFakeParameter(
+            IMyInterface fake,
+            IFoo parameter,
+            int result)
+        {
+            "Given a fake"
+                .x(() => fake = A.Fake<IMyInterface>());
+
+            "And a strict fake parameter"
+                .x(() => parameter = A.Fake<IFoo>(o => o.Strict()));
+
+            "And the fake is configured to return a value when called with the parameter"
+                .x(() => A.CallTo(() => fake.GetIt(parameter)).Returns(42));
+
+            "When I call the fake with the parameter"
+                .x(() => result = fake.GetIt(parameter));
+
+            "Then it returns the configured value"
+                .x(() => result.Should().Be(42));
+        }
+
+        [Scenario]
+        public static void CallMatchingFailureWithStrictFakeParameter(
+            IMyInterface fake,
+            IFoo parameter1,
+            IFoo parameter2,
+            int result)
+        {
+            "Given a fake"
+                .x(() => fake = A.Fake<IMyInterface>());
+
+            "And a strict fake parameter"
+                .x(() => parameter1 = A.Fake<IFoo>(o => o.Strict()));
+
+            "And another strict fake parameter"
+                .x(() => parameter2 = A.Fake<IFoo>(o => o.Strict()));
+
+            "And the fake is configured to return a value when called with the first parameter"
+                .x(() => A.CallTo(() => fake.GetIt(parameter1)).Returns(42));
+
+            "When I call the fake with the second parameter"
+                .x(() => result = fake.GetIt(parameter2));
+
+            "Then it doesn't return the configured value"
+                .x(() => result.Should().Be(0));
         }
     }
 }
