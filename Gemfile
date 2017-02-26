cb2db7b0700508e542fd09ad768d55345b7f9681
Blair Conrad <blair@blairconrad.com>
2017-02-26 14:20:24 -0500

    Use simple-targets-csx to build

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'http://rubygems.org'
-  gem 'albacore', '0.3.6'
-  gem 'rake'
-  gem 'zip'
--- build.cmd
+++ build.cmd
@@ -1,3 +1,34 @@
-@echo off
+@echo Off
 
-call bundle exec rake -f rakefile.rb %*
+rem change working directory to the repository root
+pushd %~dp0
+setlocal
+
+rem options
+set NUGET_VERSION=3.5.0
+
+rem determine nuget cache dir
+set NUGET_CACHE_DIR=%LocalAppData%\.nuget\v%NUGET_VERSION%
+
+rem download nuget to cache dir
+set NUGET_URL=https://dist.nuget.org/win-x86-commandline/v%NUGET_VERSION%/NuGet.exe
+if not exist %NUGET_CACHE_DIR%\NuGet.exe (
+  if not exist %NUGET_CACHE_DIR% md %NUGET_CACHE_DIR%
+  echo Downloading '%NUGET_URL%'' to '%NUGET_CACHE_DIR%\NuGet.exe'...
+  @powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest '%NUGET_URL%' -OutFile '%NUGET_CACHE_DIR%\NuGet.exe'"
+)
+
+rem copy nuget locally
+if not exist .nuget\NuGet.exe (
+  if not exist .nuget md .nuget
+  copy %NUGET_CACHE_DIR%\NuGet.exe .nuget\NuGet.exe > nul
+)
+
+rem restore packages for build script
+.nuget\NuGet.exe restore .\packages.config -PackagesDirectory .\packages -Verbosity quiet
+
+rem run build script
+"%ProgramFiles(x86)%\MSBuild\14.0\Bin\csi.exe" .\build.csx %*
+
+rem return to original working directory
+popd
