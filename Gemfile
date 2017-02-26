bd0db91b173194cf54faa5900710854a6276ff53
Blair Conrad <blair@blairconrad.com>
2017-02-26 12:48:55 -0500

    Remove milestone/release/version admin targets from rakefile

--- Gemfile
+++ Gemfile
@@ -1,6 +1,4 @@
 source 'http://rubygems.org'
   gem 'albacore', '0.3.6'
-  gem 'netrc'
-  gem 'octokit'
   gem 'rake'
   gem 'zip'
--- rakefile.rb
+++ rakefile.rb
@@ -57,69 +57,6 @@ approval_tests = [
   "tests/FakeItEasy.Tests.Approval.netstd/bin/Release/FakeItEasy.Tests.Approval.netstd.dll"
 ]
 
-repo = 'FakeItEasy/FakeItEasy'
-appveyor_server_url = 'https://ci.appveyor.com/project/FakeItEasy/fakeiteasy/settings'
-release_issue_labels = ['P2', 'build', 'documentation']
-release_issue_rtm_steps = <<-eos
-Can be labelled **ready** when all other issues on this milestone are closed.
-
-- [ ] run code analysis in VS in *Release* mode and address violations (send a regular PR which must be merged before continuing)
-- [ ] change `VERSION_SUFFIX` in [AppVeyor](#{appveyor_server_url}) to "" and initiate a build
-- [ ] check build
-- edit draft release in [GitHub UI](https://github.com/FakeItEasy/FakeItEasy/releases):
-    - [ ] ensure completeness of release notes, including non-owner contributors, if any (move release notes forward from any pre-releases to the current release)
-    - [ ] attach main nupkg and analyzer nupkg
-    - [ ] publish the release
-- [ ] push nupkgs to NuGet using the Deploy option on the AppVeyor build
-- [ ] tweet, mentioning contributors
-- [ ] post link to tweet as comment here for easy retweeting ;-)
-- [ ] post tweet in [Gitter](https://gitter.im/FakeItEasy/FakeItEasy)
-- [ ] post a link to the GitHub Release in each issue in this milestone, with thanks to contributors
-- [ ] run `build.cmd next_version[new_version]` to
-    - create a pull request that changes the version in CommonAssemblyInfo.cs to the expected version (of form _xx.yy.zz_)
-    - create a new draft GitHub Release
-    - create a new milestone for the next release
-    - create a new issue (like this one) for the next release, adding it to the new milestone
-- [ ] close this milestone
-- [ ] close this issue
-eos
-
-release_issue_prerelease_steps = <<-eos
-Can be labelled **ready** when all other issues destined for this release are closed.
-
-- [ ] run code analysis in VS in *Release* mode and address violations (send a regular PR which must be merged before continuing)
-- [ ] change `VERSION_SUFFIX` in [AppVeyor](#{appveyor_server_url}) to "-%version_suffix%" and initiate a build
-- [ ] check build
-- edit draft release in [GitHub UI](https://github.com/FakeItEasy/FakeItEasy/releases):
-    - [ ] ensure completeness of release notes, including non-owner contributors, if any
-    - [ ] attach main nupkg and analyzer nupkg
-    - [ ] publish the release
-- [ ] push nupkgs to NuGet using the Deploy option on the AppVeyor build
-- [ ] tweet, mentioning contributors
-- [ ] post link to tweet as comment here for easy retweeting ;-)
-- [ ] post tweet in [Gitter](https://gitter.im/FakeItEasy/FakeItEasy)
-- [ ] if another pre-release is planned, use the `pre_release` task (e.g. `build.cmd pre_release[beta002]`) to
-    - create a new draft GitHub Release
-    - create a new issue (like this one) for the next release, adding it to the next RTM milestone
-- [ ] close this issue
-eos
-
-release_body = <<-eos
-### Changed
-* _&lt;Description of change.&gt;_ (#_&lt;issue number&gt;_)
-
-### New
-* _&lt;Description of feature.&gt;_ (#_&lt;issue number&gt;_)
-
-### Fixed
-* _&lt;Description of fix.&gt;_ (#_&lt;issue number&gt;_)
-
-### With special thanks for contributions to this release from:
-* _&lt;user's actual name&gt;_ - @_&lt;github userid&gt;_
-eos
-
-ssl_cert_filename = "cacert.pem"
-
 Albacore.configure do |config|
   config.log_level = :verbose
 end
@@ -151,110 +88,6 @@ directory logs
 desc "Clean solution"
 task :clean => [logs] do
   run_msbuild solution, "Clean", msbuild_command
-end
-
-desc "Update version number and create pull request, milestone, release, and release checklist issue"
-task :next_version, :new_version do |asm, args|
-  new_version = args.new_version or
-    fail "ERROR: A new version is required, e.g.: build.cmd next_version[2.3.0]"
-
-  current_branch = `git rev-parse --abbrev-ref HEAD`.strip()
-
-  if current_branch != 'master'
-    fail "ERROR: Current branch is '#{current_branch}'. Must be on branch 'master' to set new version."
-  end if
-
-  new_branch = "set-version-to-" + new_version
-
-  require 'octokit'
-
-  use_ssl_cert_file(ssl_cert_filename)
-
-  client = Octokit::Client.new(:netrc => true)
-
-  puts "Creating branch '#{new_branch}'..."
-  `git checkout -b #{new_branch}`
-  puts "Created branch '#{new_branch}'."
-
-  puts "Setting version to '#{new_version}' in '#{assembly_info}'..."
-  Rake::Task["set_version_in_assemblyinfo"].invoke(new_version)
-  puts "Set version to '#{new_version}' in '#{assembly_info}'."
-
-  puts "Committing '#{assembly_info}'..."
-  `git commit -m "setting version to #{new_version}" #{assembly_info}`
-  puts "Committed '#{assembly_info}'."
-
-  puts "Pushing '#{new_branch}' to origin..."
-  `git push origin #{new_branch}"`
-  puts "Pushed '#{new_branch}' to origin."
-
-  puts "Creating pull request..."
-  pull_request = client.create_pull_request(
-    repo,
-    "master",
-    "#{client::user.login}:#{new_branch}",
-    "set version to #{new_version} for next release",
-    "preparing for #{new_version}"
-  )
-  puts "Created pull request \##{pull_request.number} '#{pull_request.title}'."
-
-  puts "Creating milestone '#{new_version}'..."
-  milestone = client.create_milestone(
-    repo,
-    new_version,
-    :description => new_version + ' release'
-    )
-  puts "Created milestone '#{new_version}'."
-
-  create_release(
-                 client,
-                 repo,
-                 milestone,
-                 new_version,
-                 release_body,
-                 release_issue_rtm_steps,
-                 release_issue_labels)
-end
-
-desc "Create GitHub Release and release checklist issue for pre-release build"
-task :pre_release, :version_suffix do |asm, args|
-  version_suffix = args.version_suffix or
-    fail 'ERROR: A version suffix is required, e.g.: build.cmd pre_release[beta001]'
-
-  require 'octokit'
-
-  use_ssl_cert_file(ssl_cert_filename)
-
-  client = Octokit::Client.new(:netrc => true)
-
-  milestone = client
-    .list_milestones(repo, :state => 'open')
-    .select { |m| m.title == version }
-    .first or
-    raise "ERROR: can't find existing milestone #{version}"
-
-  create_release(
-                 client,
-                 repo,
-                 milestone,
-                 "#{version}-#{version_suffix}",
-                 release_body,
-                 release_issue_prerelease_steps.sub('%version_suffix%', version_suffix),
-                 release_issue_labels)
-end
-
-desc "Update assembly info"
-assemblyinfo :set_version_in_assemblyinfo, :new_version do |asm, args|
-  new_version = args.new_version
-
-  # not using asm.version and asm.file_version due to StyleCop violations
-  asm.custom_attributes = {
-    :AssemblyVersion => new_version,
-    :AssemblyFileVersion => new_version,
-    :AssemblyInformationalVersion => new_version
-  }
-  asm.input_file = assembly_info
-  asm.output_file = assembly_info
 end
 
 desc "Build solution"
@@ -305,29 +138,6 @@ desc "create the analyzer nuget package"
 exec :pack => [:build, output] do |cmd|
   cmd.command = nuget_command
   cmd.parameters "pack #{analyzer_nuspec} -Version #{version}#{version_suffix}#{build_suffix} -OutputDirectory #{output}"
-end
-
-def create_release(client, repo, milestone, release_version, release_body, release_issue_body, release_issue_labels)
-  release_description = release_version + ' release'
-  puts "Creating issue '#{release_description}'..."
-  issue = client.create_issue(
-    repo,
-    release_description,
-    release_issue_body,
-    :labels => release_issue_labels,
-    :milestone => milestone.number
-    )
-  puts "Created issue \##{issue.number} '#{release_description}'."
-
-  puts "Creating release '#{release_version}'..."
-  client.create_release(
-    repo,
-    release_version,
-    :name => release_version,
-    :draft => true,
-    :body => release_body
-    )
-  puts "Created release '#{release_version}'."
 end
 
 def print_vars(variables)
@@ -405,12 +215,4 @@ def restore_dependencies_for_project(project_dir)
   restore_cmd.command = "dotnet"
   restore_cmd.parameters "restore #{project_dir}"
   restore_cmd.execute
-end
-
-# Load SSL cert file to enable Ruby SSL support.
-# New cert files can be had from
-#    http://curl.haxx.se/ca/cacert.pem
-# in case the current one stops working.
-def use_ssl_cert_file(ssl_cert_filename)
-  ENV["SSL_CERT_FILE"] = File.absolute_path(ssl_cert_filename)
 end
