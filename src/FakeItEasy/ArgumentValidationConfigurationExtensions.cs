bb6ce3816c2ec576a944f2dec599aaa4971b9b12
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2017-03-13 23:07:51 +0100

    Add non implemented strongly-typed overloads for WhenArgumentsMatch

--- src/FakeItEasy/ArgumentValidationConfigurationExtensions.cs
+++ src/FakeItEasy/ArgumentValidationConfigurationExtensions.cs
@@ -1,5 +1,6 @@
 namespace FakeItEasy
 {
+    using System;
     using FakeItEasy.Configuration;
 
     /// <summary>
@@ -19,6 +20,72 @@ namespace FakeItEasy
             Guard.AgainstNull(configuration, nameof(configuration));
 
             return configuration.WhenArgumentsMatch(x => true);
+        }
+
+        /// <summary>
+        /// Configures the call to be accepted when the specified predicate returns true.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="T1">The type of the first argument.</typeparam>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="argumentsPredicate">The argument predicate.</param>
+        /// <returns>A configuration object.</returns>
+        public static TInterface WhenArgumentsMatch<TInterface, T1>(
+            this IArgumentValidationConfiguration<TInterface> configuration,
+            Func<T1, bool> argumentsPredicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Configures the call to be accepted when the specified predicate returns true.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="T1">The type of the first argument.</typeparam>
+        /// <typeparam name="T2">The type of the second argument.</typeparam>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="argumentsPredicate">The argument predicate.</param>
+        /// <returns>A configuration object.</returns>
+        public static TInterface WhenArgumentsMatch<TInterface, T1, T2>(
+            this IArgumentValidationConfiguration<TInterface> configuration,
+            Func<T1, T2, bool> argumentsPredicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Configures the call to be accepted when the specified predicate returns true.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="T1">The type of the first argument.</typeparam>
+        /// <typeparam name="T2">The type of the second argument.</typeparam>
+        /// <typeparam name="T3">The type of the third argument.</typeparam>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="argumentsPredicate">The argument predicate.</param>
+        /// <returns>A configuration object.</returns>
+        public static TInterface WhenArgumentsMatch<TInterface, T1, T2, T3>(
+            this IArgumentValidationConfiguration<TInterface> configuration,
+            Func<T1, T2, T3, bool> argumentsPredicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Configures the call to be accepted when the specified predicate returns true.
+        /// </summary>
+        /// <typeparam name="TInterface">The type of the interface.</typeparam>
+        /// <typeparam name="T1">The type of the first argument.</typeparam>
+        /// <typeparam name="T2">The type of the second argument.</typeparam>
+        /// <typeparam name="T3">The type of the third argument.</typeparam>
+        /// <typeparam name="T4">The type of the fourth argument.</typeparam>
+        /// <param name="configuration">The configuration.</param>
+        /// <param name="argumentsPredicate">The argument predicate.</param>
+        /// <returns>A configuration object.</returns>
+        public static TInterface WhenArgumentsMatch<TInterface, T1, T2, T3, T4>(
+            this IArgumentValidationConfiguration<TInterface> configuration,
+            Func<T1, T2, T3, T4, bool> argumentsPredicate)
+        {
+            throw new NotImplementedException();
         }
     }
 }
