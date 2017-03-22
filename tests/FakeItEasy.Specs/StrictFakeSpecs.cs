496554fcd1a54761f60f417f457eb906c66543cf
Blair Conrad <blair@blairconrad.com>
2017-03-22 15:35:40 -0400

    Add spec for message for unconfigured call to method on strict Fake

--- tests/FakeItEasy.Specs/StrictFakeSpecs.cs
+++ tests/FakeItEasy.Specs/StrictFakeSpecs.cs
@@ -11,6 +11,52 @@ namespace FakeItEasy.Specs
         public interface IMyInterface
         {
             void DoIt(Guid id);
+
+            Guid MakeIt(string name);
+        }
+
+        [Scenario]
+        public static void ArgumentMismatchVoid(
+            IMyInterface fake,
+            Exception exception)
+        {
+            "Given a strict fake"
+                .x(() => fake = A.Fake<IMyInterface>(o => o.Strict()));
+
+            "And I configure the fake to do nothing when a void method is called with certain arguments"
+                .x(() => A.CallTo(() => fake.DoIt(Guid.Empty)).DoesNothing());
+
+            "When I call the method with non-matching arguments"
+                .x(() => exception = Record.Exception(() => fake.DoIt(new Guid("a762f030-d39e-4316-92b1-a51eff3dc51f"))));
+
+            "Then the fake throws an expectation exception"
+                .x(() => exception.Should().BeAnExceptionOfType<ExpectationException>());
+
+            "And the exception message describes the call"
+                .x(() => exception.Message.Should().Be(
+                    "Call to unconfigured method of strict fake: FakeItEasy.Specs.StrictFakeSpecs+IMyInterface.DoIt(id: a762f030-d39e-4316-92b1-a51eff3dc51f)."));
+        }
+
+        [Scenario]
+        public static void ArgumentMismatchNonVoid(
+            IMyInterface fake,
+            Exception exception)
+        {
+            "Given a strict fake"
+                .x(() => fake = A.Fake<IMyInterface>(o => o.Strict()));
+
+            "And I configure the fake to return a value when a non-void method is called with certain arguments"
+                .x(() => A.CallTo(() => fake.MakeIt("empty")).Returns(Guid.Empty));
+
+            "When I call the method with non-matching arguments"
+                .x(() => exception = Record.Exception(() => fake.MakeIt("something")));
+
+            "Then the fake throws an expectation exception"
+                .x(() => exception.Should().BeAnExceptionOfType<ExpectationException>());
+
+            "And the exception message describes the call"
+                .x(() => exception.Message.Should().Be(
+                    @"Call to unconfigured method of strict fake: FakeItEasy.Specs.StrictFakeSpecs+IMyInterface.MakeIt(name: ""something"")."));
         }
 
         [Scenario]
