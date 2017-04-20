6cf49427a8e8010ffdd72f66c0819d8f71f5e6d5
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2017-04-20 21:18:46 +0200

    Implement Raise.FreeForm.With

--- src/FakeItEasy/Core/DelegateRaiser.cs
+++ src/FakeItEasy/Core/DelegateRaiser.cs
@@ -24,7 +24,7 @@ namespace FakeItEasy.Core
         {
             var delegateMethod = typeof(TEventHandler).GetMethod("Invoke");
 
-            AssertThatValuesSatisfyCallSignature(delegateMethod, arguments);
+            ValueProducerSignatureHelper.AssertThatValuesSatisfyCallSignature(delegateMethod, arguments);
 
             this.EventHandler = A.Fake<TEventHandler>();
             this.EventArguments = arguments;
@@ -52,54 +52,6 @@ namespace FakeItEasy.Core
         object[] IEventRaiserArgumentProvider.GetEventArguments(object fake)
         {
             return this.EventArguments;
-        }
-
-        private static void AssertThatValuesSatisfyCallSignature(MethodInfo callMethod, object[] values)
-        {
-            if (IsCallSignatureSatisfiedByValues(callMethod, values))
-            {
-                return;
-            }
-
-            var formatter = ServiceLocator.Current.Resolve<ArgumentValueFormatter>();
-
-            var fakeSignature = BuildSignatureDescription(callMethod.GetParameters().Select(p => (object)p.ParameterType), formatter);
-            var actionSignature = BuildSignatureDescription(values.Select(v => v == null ? null : (object)v.GetType()), formatter);
-
-            throw new FakeConfigurationException(
-                $"The event has the signature ({fakeSignature}), but the provided arguments have types ({actionSignature}).");
-        }
-
-        private static bool IsCallSignatureSatisfiedByValues(MethodInfo callMethod, object[] values)
-        {
-            var callMethodParameterTypes = callMethod.GetParameters().Select(p => p.ParameterType).ToList();
-
-            if (callMethodParameterTypes.Count != values.Length)
-            {
-                return false;
-            }
-
-            for (int i = 0; i < callMethodParameterTypes.Count; i++)
-            {
-                if (values[i] == null)
-                {
-                    if (callMethodParameterTypes[i].GetTypeInfo().IsValueType)
-                    {
-                        return false;
-                    }
-                }
-                else if (!callMethodParameterTypes[i].IsInstanceOfType(values[i]))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private static string BuildSignatureDescription(IEnumerable<object> types, ArgumentValueFormatter formatter)
-        {
-            return types.ToCollectionString(formatter.GetArgumentValueAsString, ", ");
         }
     }
 }
--- src/FakeItEasy/ValueProducerSignatureHelper.cs
+++ src/FakeItEasy/ValueProducerSignatureHelper.cs
@@ -1,9 +1,11 @@
 namespace FakeItEasy
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
 
     using FakeItEasy.Configuration;
+    using FakeItEasy.Core;
 
     /// <summary>
     /// Provides helper methods for checking the value producer signature against call signatures.
@@ -20,6 +22,22 @@ namespace FakeItEasy
                 throw new FakeConfigurationException(
                     $"The faked method has the signature ({fakeSignature}), but {nameOfFeature} was used with ({actionSignature}).");
             }
+        }
+
+        internal static void AssertThatValuesSatisfyCallSignature(MethodInfo callMethod, object[] values)
+        {
+            if (IsCallSignatureSatisfiedByValues(callMethod, values))
+            {
+                return;
+            }
+
+            var formatter = ServiceLocator.Current.Resolve<ArgumentValueFormatter>();
+
+            var fakeSignature = BuildSignatureDescription(callMethod.GetParameters().Select(p => (object)p.ParameterType), formatter);
+            var actionSignature = BuildSignatureDescription(values.Select(v => v == null ? null : (object)v.GetType()), formatter);
+
+            throw new FakeConfigurationException(
+                $"The event has the signature ({fakeSignature}), but the provided arguments have types ({actionSignature}).");
         }
 
         private static bool IsCallSignatureSatisfiedByValueProducerSignature(MethodInfo callMethod, MethodInfo valueProducerMethod)
@@ -45,9 +63,41 @@ namespace FakeItEasy
             return true;
         }
 
+        private static bool IsCallSignatureSatisfiedByValues(MethodInfo callMethod, object[] values)
+        {
+            var callMethodParameterTypes = callMethod.GetParameters().Select(p => p.ParameterType).ToList();
+
+            if (callMethodParameterTypes.Count != values.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < callMethodParameterTypes.Count; i++)
+            {
+                if (values[i] == null)
+                {
+                    if (callMethodParameterTypes[i].GetTypeInfo().IsValueType)
+                    {
+                        return false;
+                    }
+                }
+                else if (!callMethodParameterTypes[i].IsInstanceOfType(values[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         private static string BuildSignatureDescription(MethodInfo method)
         {
             return method.GetParameters().ToCollectionString(x => x.ParameterType.ToString(), ", ");
+        }
+
+        private static string BuildSignatureDescription(IEnumerable<object> types, ArgumentValueFormatter formatter)
+        {
+            return types.ToCollectionString(formatter.GetArgumentValueAsString, ", ");
         }
     }
 }
