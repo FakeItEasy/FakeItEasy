626f41dcf4d9b4f8991faa1b71d1925f93a1c4c4
Blair Conrad <blair@blairconrad.com>
2017-05-12 12:29:29 -0400

    Dynamically generate external extension point assembly

--- tests/FakeItEasy.IntegrationTests/TypeCatalogueTests.cs
+++ tests/FakeItEasy.IntegrationTests/TypeCatalogueTests.cs
@@ -1,10 +1,9 @@
-namespace FakeItEasy.IntegrationTests
+﻿namespace FakeItEasy.IntegrationTests
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Linq;
-    using System.Text;
 #if FEATURE_NETCORE_REFLECTION
     using System.Reflection;
 #endif
@@ -12,8 +11,15 @@ namespace FakeItEasy.IntegrationTests
     using FluentAssertions;
     using Xunit;
 
-    public class TypeCatalogueTests
+    public class TypeCatalogueTests : IClassFixture<ExternalAssemblyGenerator>
     {
+        public TypeCatalogueTests(ExternalAssemblyGenerator externalAssemblyGenerator)
+        {
+            this.externalAssemblyGenerator = externalAssemblyGenerator;
+        }
+
+        private ExternalAssemblyGenerator externalAssemblyGenerator;
+
         [SuppressMessage("Microsoft.Usage", "CA2202:Do not dispose objects multiple times", Justification = "No exception is thrown.")]
 #if FEATURE_NETCORE_REFLECTION
         [Fact(Skip = "Loading assemblies from duplicate files does not throw an error on this platform.")]
@@ -23,20 +29,16 @@ namespace FakeItEasy.IntegrationTests
         public void Should_warn_of_duplicate_input_assemblies_with_different_paths()
         {
             // Arrange
-            var originalExternalDll = this.GetPathToOriginalExternalDll();
-            var copyOfExternalDll = this.GetPathToCopyOfExternalDll();
-            File.Copy(originalExternalDll, copyOfExternalDll, overwrite: true);
-
             var expectedMessageFormat =
 @"*Warning: FakeItEasy failed to load assembly '*{0}' while scanning for extension points. Any IArgumentValueFormatters, IDummyFactories, and IFakeOptionsBuilders in that assembly will not be available.*";
-            var expectedMessage = string.Format(expectedMessageFormat, copyOfExternalDll);
+            var expectedMessage = string.Format(expectedMessageFormat, this.externalAssemblyGenerator.AssemblyCopyPath);
 
             var catalogue = new TypeCatalogue();
 
             // Act
             var actualMessage = CaptureConsoleOutput(() => catalogue.Load(new[]
             {
-                originalExternalDll, copyOfExternalDll
+                this.externalAssemblyGenerator.AssemblyOriginalPath, this.externalAssemblyGenerator.AssemblyCopyPath
             }));
 
             // Assert
@@ -106,7 +108,7 @@ namespace FakeItEasy.IntegrationTests
             var catalogue = new TypeCatalogue();
 
             // Act
-            catalogue.Load(new[] { this.GetPathToOriginalExternalDll() });
+            catalogue.Load(new[] { this.externalAssemblyGenerator.AssemblyOriginalPath });
 
             // Assert
             catalogue.GetAvailableTypes().Select(type => type.FullName).Should().Contain("FakeItEasy.IntegrationTests.External.GuidValueFormatter");
@@ -145,22 +147,6 @@ namespace FakeItEasy.IntegrationTests
                 writer.Flush();
                 return writer.Encoding.GetString(stream.ToArray());
             }
-        }
-
-        private string GetPathToOriginalExternalDll()
-        {
-            var executingAssemblyPath = new Uri(this.GetType().GetTypeInfo().Assembly.CodeBase).LocalPath
-                .Replace("FakeItEasy.IntegrationTests", "FakeItEasy.IntegrationTests.External")
-                .Replace("netcoreapp1.0", "netstandard1.6");
-            return Path.GetFullPath(executingAssemblyPath);
-        }
-
-        private string GetPathToCopyOfExternalDll()
-        {
-            var executingAssemblyPath = new Uri(this.GetType().GetTypeInfo().Assembly.Location).LocalPath;
-            var executingAssemblyDirectory = new DirectoryInfo(executingAssemblyPath).Parent.FullName;
-            return Path.GetFullPath(Path.Combine(
-                executingAssemblyDirectory, "FakeItEasy.IntegrationTests.External.dll"));
         }
     }
 }
