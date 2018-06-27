515599864cc93ef2671b560ecb58fb371e8c92f5
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2018-06-27 11:49:18 +0000

    [red] Document behavior for exception in call count constraint

--- tests/FakeItEasy.Specs/UserCallbackExceptionSpecs.cs
+++ tests/FakeItEasy.Specs/UserCallbackExceptionSpecs.cs
@@ -79,6 +79,28 @@ namespace FakeItEasy.Specs
                 .x(() => exception.InnerException.Should().BeAnExceptionOfType<MyException>().Which.Message.Should().Be("Oops"));
         }
 
+        [Scenario]
+        public void ExceptionInCallCountSpecification(IFoo fake, Exception exception)
+        {
+            "Given a fake"
+                .x(() => fake = A.Fake<IFoo>());
+
+            "And no call to the fake is made"
+                .x(() => { });
+
+            "When an assertion is made with a call count constraint that throws an exception"
+                .x(() => exception = Record.Exception(() => A.CallTo(() => fake.Bar(0)).MustHaveHappenedANumberOfTimesMatching(n => ThrowException())));
+
+            "Then a UserCallbackException should be thrown"
+                .x(() => exception.Should().BeAnExceptionOfType<UserCallbackException>());
+
+            "And its message should describe where the exception was thrown from"
+                .x(() => exception.Message.Should().Be("Call count constraint <a number of times matching the predicate 'n => ThrowException()'> threw an exception. See inner exception for details."));
+
+            "And the inner exception should be the original exception"
+                .x(() => exception.InnerException.Should().BeAnExceptionOfType<MyException>().Which.Message.Should().Be("Oops"));
+        }
+
         private static bool ThrowException()
         {
             throw new MyException("Oops");
