e4b5235f834d64ddb615361c903a42b246bd96bb
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2018-08-19 13:16:38 +0200

    Refactor run-csx.ps1 to use functions

--- tools/run-csx.ps1
+++ tools/run-csx.ps1
@@ -1,54 +1,98 @@
+# Treat all errors as terminating
+$ErrorActionPreference = "Stop"
+
 $ToolsDir = $PSScriptRoot
-
-# change working directory to the repository root
-Push-Location $ToolsDir\..
-
+$RepoRoot = "$ToolsDir\.."
 $NuGetVersion = "4.1.0"
-
-# determine nuget cache dir
 $NuGetCacheDir = "$($Env:LOCALAPPDATA)\.nuget\v$NuGetVersion"
 $NuGetCacheFile = "$NuGetCacheDir\NuGet.exe"
-
-# download nuget to cache dir
-$NuGetUrl = "https://dist.nuget.org/win-x86-commandline/v$NuGetVersion/NuGet.exe"
-if (!(Test-Path $NuGetCacheFile)) {
-    if (!(Test-Path $NuGetCacheDir)) {
-        New-Item -ItemType Directory $NuGetCacheDir | Out-Null
-    }
-    Write-Host "Downloading '$NuGetUrl' to '$NuGetCacheFile'..."
-    Invoke-WebRequest $NuGetUrl -OutFile $NuGetCacheFile
-}
-
-# copy nuget locally
 $NuGetDir = "$ToolsDir\.nuget"
 $NuGetExe = "$NuGetDir\NuGet.exe"
-if (!(Test-Path $NuGetDir)) {
-    New-Item -ItemType Directory $NuGetDir | Out-Null
-}
-Copy-Item $NuGetCacheFile $NuGetExe
-
-$PackagesConfig = "$ToolsDir\packages.config"
 $PackagesDir = "$ToolsDir\packages"
-
-# check if packages have changed
+$PackagesConfig = "$ToolsDir\packages.config"
 $PackagesHashFile = "$ToolsDir\packages.config.sha1"
-$PreviousPackagesHash = ""
-if (Test-Path $PackagesHashFile) {
-    $PreviousPackagesHash = (Get-Content $PackagesHashFile).Trim()
-}
-$PackagesHash = (Get-FileHash -Algorithm SHA1 -Path $PackagesConfig).Hash
-# if packages.config has changed, clear packages and store new hash
-if ($PackagesHash -ne $PreviousPackagesHash) {
-    Write-Host "$PackagesConfig has changed, reinstalling packages..."
-    Remove-Item -Recurse -Force $PackagesDir
-    Set-Content $PackagesHashFile $PackagesHash
+$CsiExe = "$PackagesDir\Microsoft.Net.Compilers\tools\csi.exe"
+
+function EnsureDirectoryExists($path)
+{
+    if (!(Test-Path $path -PathType Container)) {
+        New-Item -ItemType Directory  $path | Out-Null
+    }
 }
 
-# restore packages for build script
-& $NuGetExe install $PackagesConfig -OutputDirectory $PackagesDir -Verbosity quiet -ExcludeVersion
+function DownloadNuGetToCache()
+{
+    $NuGetUrl = "https://dist.nuget.org/win-x86-commandline/v$NuGetVersion/NuGet.exe"
+    if (!(Test-Path $NuGetCacheFile)) {
+        EnsureDirectoryExists($NuGetCacheDir)
+        Write-Host "Downloading '$NuGetUrl' to '$NuGetCacheFile'..."
+        Invoke-WebRequest $NuGetUrl -OutFile $NuGetCacheFile
+    }
+}
+
+function CopyNuGetLocally()
+{
+    EnsureDirectoryExists($NuGetDir)
+    Copy-Item $NuGetCacheFile $NuGetExe
+}
+
+function ReadPreviousPackagesHash()
+{
+    if (Test-Path $PackagesHashFile)
+    {
+        (Get-Content $PackagesHashFile).Trim()
+    }
+    else
+    {
+        ""
+    }
+}
+
+function WritePackagesHash($hash)
+{
+    Set-Content $PackagesHashFile $hash
+}
+
+function ComputePackagesHash()
+{
+    (Get-FileHash -Algorithm SHA1 -Path $PackagesConfig).Hash
+}
+
+function ClearPackagesDir()
+{
+    if (Test-Path -Path $PackagesDir -PathType Container)
+    {
+        Write-Host "$PackagesConfig has changed, clearing packages to force reinstall..."
+        Remove-Item -Recurse -Force $PackagesDir
+    }
+}
+
+function ClearPackagesIfChanged()
+{
+    # check if packages.config have changed
+    $PreviousPackagesHash = ReadPreviousPackagesHash
+    $PackagesHash = ComputePackagesHash
+    if ($PackagesHash -ne $PreviousPackagesHash) {
+        # if packages.config has changed, clear packages and store new hash
+        ClearPackagesDir
+        WritePackagesHash($PackagesHash)
+    }
+}
+
+function RestorePackages()
+{
+    & $NuGetExe install $PackagesConfig -OutputDirectory $PackagesDir -Verbosity quiet -ExcludeVersion
+}
+
+# change working directory to the repository root
+Push-Location $RepoRoot
+
+DownloadNuGetToCache
+CopyNuGetLocally
+ClearPackagesIfChanged
+RestorePackages
 
 # run build script
-$CsiExe = "$PackagesDir\Microsoft.Net.Compilers\tools\csi.exe"
 & $CsiExe $args
 
 # return to original working directory
