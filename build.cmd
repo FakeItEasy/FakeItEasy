14763de0e70b934f866ee70979b8df2faf508afd
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2018-08-28 13:24:14 +0200

    Convert build script to csproj

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-@powershell -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Unrestricted -Command "%~dp0\tools\run-csx.ps1 %~dp0\tools\build.csx %*"
+@dotnet run --project "%~dp0\tools\FakeItEasy.Build\FakeItEasy.Build.csproj" -- %*
