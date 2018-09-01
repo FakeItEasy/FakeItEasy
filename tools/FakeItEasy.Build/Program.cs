1209877ae7ac5efbf80a94c74956aa6469e31422
Adam Ralph <adam@adamralph.com>
2018-09-01 10:30:42 +0200

    remove redundant using

--- tools/FakeItEasy.Build/Program.cs
+++ tools/FakeItEasy.Build/Program.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
-    using System.Runtime.CompilerServices;
     using static Bullseye.Targets;
     using static SimpleExec.Command;
 
