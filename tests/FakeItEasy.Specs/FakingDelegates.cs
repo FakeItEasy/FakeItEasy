0912946aa4fe7069db8404246c994f81ebe28198
Blair Conrad <blair@blairconrad.com>
2018-11-11 14:52:43 -0500

    Convert language in FakingDelegates specs

--- tests/FakeItEasy.Specs/FakingDelegates.cs
+++ tests/FakeItEasy.Specs/FakingDelegates.cs
@@ -8,78 +8,82 @@ namespace FakeItEasy.Specs
     public static class FakingDelegates
     {
         [Scenario]
-        public static void WithoutConfiguration(
-            Func<string, int> fakedDelegate)
+        public static void WithoutConfiguration(Func<string, int> fakedDelegate)
         {
-            "establish"
+            "Given a faked delegate"
                 .x(() => fakedDelegate = A.Fake<Func<string, int>>());
 
-            "when faking a delegate type and invoking without configuration"
+            "When I invoke it via the Invoke method"
                 .x(() => fakedDelegate.Invoke("foo"));
 
-            "it should be possible to assert the call"
+            "Then an assertion that Invoke was called passes"
                 .x(() => A.CallTo(() => fakedDelegate.Invoke("foo")).MustHaveHappened());
 
-            "it should be possible to assert the call without specifying invoke method"
+            "And an assertion that the delegate was called without mentioning Invoke passes"
                 .x(() => A.CallTo(() => fakedDelegate("foo")).MustHaveHappened());
         }
 
         [Scenario]
-        public static void WithConfiguration(
-            Func<string, int> fakedDelegate,
-            int result)
+        public static void ConfiguredToReturn(Func<string, int> fakedDelegate, int result)
         {
-            "establish"
+            "Given a faked delegate"
                 .x(() => fakedDelegate = A.Fake<Func<string, int>>());
 
-            "establish"
+            "And I configure it to return 10"
                 .x(() => A.CallTo(() => fakedDelegate.Invoke(A<string>._)).Returns(10));
 
-            "when faking a delegate type and invoking with configuration"
-                .x(() => result = fakedDelegate(null));
+            "When I invoke it"
+                .x(() => result = fakedDelegate.Invoke("foo"));
 
-            "it should return configured value"
+            "Then it returns the configured value"
                 .x(() => result.Should().Be(10));
         }
 
         [Scenario]
-        public static void Throws(
-            Func<string, int> fakedDelegate,
-            FormatException expectedException,
-            Exception exception)
+        public static void ConfiguredToReturnLazily(Func<string, int> fakedDelegate, int result)
         {
-            "establish"
+            "Given a faked delegate"
                 .x(() => fakedDelegate = A.Fake<Func<string, int>>());
 
-            "establish"
-                .x(() =>
-                    {
-                        expectedException = new FormatException();
-                        A.CallTo(() => fakedDelegate.Invoke(A<string>._)).Throws(expectedException);
-                    });
+            "And I configure it to return lazily"
+                .x(() => A.CallTo(() => fakedDelegate.Invoke(A<string>._)).ReturnsLazily((string s) => Int32.Parse(s)));
 
-            "when faking a delegate type and invoking with throwing configuration"
+            "When I invoke it"
+                .x(() => result = fakedDelegate.Invoke("-27"));
+
+            "Then it returns a value constructed from the input"
+                .x(() => result.Should().Be(-27));
+        }
+
+        [Scenario]
+        public static void Throws(Func<string, int> fakedDelegate, FormatException expectedException, Exception exception)
+        {
+            "Given a faked delegate"
+                .x(() => fakedDelegate = A.Fake<Func<string, int>>());
+
+            "And I configure it to throw an exception"
+                .x(() => A.CallTo(() => fakedDelegate.Invoke(A<string>._)).Throws(expectedException = new FormatException()));
+
+            "When I invoke it"
                 .x(() => exception = Record.Exception(() => fakedDelegate(null)));
 
-            "it should throw the configured exception"
+            "Then it throws the configured exception"
                 .x(() => exception.Should().BeSameAs(expectedException));
         }
 
         [Scenario]
-        public static void MissingInvoke(
-            Func<string, int> fakedDelegate,
-            int result)
+        public static void MissingInvoke(Func<string, int> fakedDelegate, int result)
         {
-            "establish"
+            "Given a faked delegate"
                 .x(() => fakedDelegate = A.Fake<Func<string, int>>());
 
-            "establish"
+            "And I configure it to return 10 without specifying the Invoke method explicitly"
                 .x(() => A.CallTo(() => fakedDelegate(A<string>._)).Returns(10));
 
-            "when faking a delegate type and invoking with configuration without specifying invoke method"
+            "When I invoke it without specifying the Invoke method explicitly"
                 .x(() => result = fakedDelegate(null));
 
-            "it should return configured value"
+            "Then it returns the configured value"
                 .x(() => result.Should().Be(10));
         }
     }
