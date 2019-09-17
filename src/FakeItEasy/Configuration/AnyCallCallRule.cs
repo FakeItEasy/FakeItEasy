1aa387548c3c8956cef35bdd72fc2dacc8def34b
Blair Conrad <blair@blairconrad.com>
2019-09-17 12:29:32 -0400

    Collapse AnyCallCallRule.applicableToAllNonVoidReturnTypes and applicableToMembersWithReturnType

--- src/FakeItEasy/Configuration/AnyCallCallRule.cs
+++ src/FakeItEasy/Configuration/AnyCallCallRule.cs
@@ -6,39 +6,30 @@ namespace FakeItEasy.Configuration
     internal class AnyCallCallRule
         : BuildableCallRule
     {
-        private Func<ArgumentCollection, bool> argumentsPredicate;
-        private bool applicableToAllNonVoidReturnTypes;
-        private Type applicableToMembersWithReturnType;
-
-        public AnyCallCallRule()
-        {
-            this.argumentsPredicate = x => true;
-        }
+        private Func<ArgumentCollection, bool> argumentsPredicate = x => true;
+        private Type applicableToMembersWithReturnType = typeof(AllReturnTypes);
 
         public void MakeApplicableToMembersWithReturnType(Type type) => this.applicableToMembersWithReturnType = type;
 
-        public void MakeApplicableToAllNonVoidReturnTypes() => this.applicableToAllNonVoidReturnTypes = true;
+        public void MakeApplicableToAllNonVoidReturnTypes() => this.applicableToMembersWithReturnType = typeof(AllNonVoidReturnTypes);
 
         public override void DescribeCallOn(IOutputWriter writer)
         {
-            if (this.applicableToMembersWithReturnType is object)
-            {
-                if (this.applicableToMembersWithReturnType == typeof(void))
-                {
-                    writer.Write("Any call with void return type to the fake object.");
-                }
-                else
-                {
-                    writer.Write("Any call with return type ").Write(this.applicableToMembersWithReturnType).Write(" to the fake object.");
-                }
-            }
-            else if (this.applicableToAllNonVoidReturnTypes)
+            if (this.applicableToMembersWithReturnType == typeof(AllNonVoidReturnTypes))
             {
                 writer.Write("Any call with non-void return type to the fake object.");
             }
-            else
+            else if (this.applicableToMembersWithReturnType == typeof(void))
+            {
+                writer.Write("Any call with void return type to the fake object.");
+            }
+            else if (this.applicableToMembersWithReturnType == typeof(AllReturnTypes))
             {
                 writer.Write("Any call made to the fake object.");
+            }
+            else
+            {
+                writer.Write("Any call with return type ").Write(this.applicableToMembersWithReturnType).Write(" to the fake object.");
             }
         }
 
@@ -61,17 +52,17 @@ namespace FakeItEasy.Configuration
                 throw new UserCallbackException(ExceptionMessages.UserCallbackThrewAnException("Arguments predicate"), ex);
             }
 
-            if (this.applicableToMembersWithReturnType is object)
+            if (this.applicableToMembersWithReturnType == typeof(AllReturnTypes))
             {
-                return this.applicableToMembersWithReturnType == fakeObjectCall.Method.ReturnType;
+                return true;
             }
 
-            if (this.applicableToAllNonVoidReturnTypes)
+            if (this.applicableToMembersWithReturnType == typeof(AllNonVoidReturnTypes))
             {
                 return fakeObjectCall.Method.ReturnType != typeof(void);
             }
 
-            return true;
+            return this.applicableToMembersWithReturnType == fakeObjectCall.Method.ReturnType;
         }
 
         protected override BuildableCallRule CloneCallSpecificationCore() =>
@@ -79,5 +70,13 @@ namespace FakeItEasy.Configuration
             {
                 argumentsPredicate = this.argumentsPredicate
             };
+
+        private class AllNonVoidReturnTypes
+        {
+        }
+
+        private class AllReturnTypes
+        {
+        }
     }
 }
