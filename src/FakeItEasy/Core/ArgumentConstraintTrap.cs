e32bf0d27194f406e248acf033349e0aca91b429
Blair Conrad <blair@blairconrad.com>
2019-10-02 20:07:20 -0400

    Avoid using null field in ArgumentConstraintTrap

--- src/FakeItEasy/Core/ArgumentConstraintTrap.cs
+++ src/FakeItEasy/Core/ArgumentConstraintTrap.cs
@@ -3,40 +3,36 @@ namespace FakeItEasy.Core
     using System;
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
+    using System.Threading;
 
     using FakeItEasy.Configuration;
 
     internal class ArgumentConstraintTrap
         : IArgumentConstraintTrapper
     {
-        [ThreadStatic]
-        private static List<IArgumentConstraint> trappedConstraints;
-
         [SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "CallTo", Justification = "It's an identifier")]
-        public static void ReportTrappedConstraint(IArgumentConstraint constraint)
-        {
-            if (trappedConstraints is null)
-            {
-                throw new InvalidOperationException("A<T>.Ignored, A<T>._, and A<T>.That can only be used in the context of a call specification with A.CallTo()");
-            }
+        private static readonly Action<IArgumentConstraint> ThrowWhenUnpreparedToTrapConstraint = constraint =>
+            throw new InvalidOperationException("A<T>.Ignored, A<T>._, and A<T>.That can only be used in the context of a call specification with A.CallTo()");
 
-            trappedConstraints.Add(constraint);
-        }
+        private static ThreadLocal<Action<IArgumentConstraint>> saveTrappedConstraintAction =
+            new ThreadLocal<Action<IArgumentConstraint>>(() => ThrowWhenUnpreparedToTrapConstraint);
+
+        public static void ReportTrappedConstraint(IArgumentConstraint constraint) =>
+            saveTrappedConstraintAction.Value.Invoke(constraint);
 
         public IArgumentConstraint TrapConstraintOrCreate(
             Action actionThatProducesConstraint,
             Func<IArgumentConstraint> defaultConstraintFactory)
         {
-            trappedConstraints = new List<IArgumentConstraint>();
-            var result = trappedConstraints;
+            var trappedConstraints = new List<IArgumentConstraint>();
 
+            saveTrappedConstraintAction.Value = trappedConstraints.Add;
             actionThatProducesConstraint.Invoke();
+            saveTrappedConstraintAction.Value = ThrowWhenUnpreparedToTrapConstraint;
 
-            trappedConstraints = null;
-
-            return result.Count == 0 ? defaultConstraintFactory.Invoke() :
-                result.Count == 1 ? result[0] :
-                throw new FakeConfigurationException(ExceptionMessages.TooManyArgumentConstraints(result[1]));
+            return trappedConstraints.Count == 0 ? defaultConstraintFactory.Invoke() :
+                trappedConstraints.Count == 1 ? trappedConstraints[0] :
+                throw new FakeConfigurationException(ExceptionMessages.TooManyArgumentConstraints(trappedConstraints[1]));
         }
     }
 }
