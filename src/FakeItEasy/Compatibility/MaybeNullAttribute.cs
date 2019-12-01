a503377a96c1de6dc566a808fe57757bbe3fdb69
Blair Conrad <blair@blairconrad.com>
2019-12-01 12:01:05 -0500

    Make MaybeNull attribute internal

--- src/FakeItEasy/Compatibility/MaybeNullAttribute.cs
+++ src/FakeItEasy/Compatibility/MaybeNullAttribute.cs
@@ -3,7 +3,7 @@ namespace System.Diagnostics.CodeAnalysis
 {
     /// <summary>Specifies that an output may be null even if the corresponding type disallows it.</summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue, Inherited = false)]
-    public sealed class MaybeNullAttribute : Attribute
+    internal sealed class MaybeNullAttribute : Attribute
     {
     }
 }
