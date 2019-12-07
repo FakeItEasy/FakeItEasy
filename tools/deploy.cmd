ffa86e6b2c2c88ba68b2f85a7eb9776101bbc1b6
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-12-07 01:01:36 +0100

    Update submodules

--- tools/deploy.cmd
+++ tools/deploy.cmd
@@ -1,1 +1,4 @@
+@echo off
+git submodule -q update --init
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet run --project "%~dp0..\tools-shared\FakeItEasy.Deploy\FakeItEasy.Deploy.csproj" -- "%~dp0..\artifacts\output" %*
--- tools/prepare_release.cmd
+++ tools/prepare_release.cmd
@@ -1,1 +1,4 @@
+@echo off
+git submodule -q update --init
+if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet run --project "%~dp0..\tools-shared\FakeItEasy.PrepareRelease\FakeItEasy.PrepareRelease.csproj" -- FakeItEasy %*
