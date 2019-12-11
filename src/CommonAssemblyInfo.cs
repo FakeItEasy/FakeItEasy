30ba25b9f7d63d8240949d5fac3cd44ffefe0064
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2019-12-11 12:22:07 +0100

    Remove unnecessary assembly attributes

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,8 +1,3 @@
-using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ComVisible(false)]
