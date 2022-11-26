ad5702d2c72cda4ade497d7f0e99635533d498c8
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2022-11-26 15:33:20 -0500

    Add CI workflow

---
+++ build.ps1
@@ -0,0 +1,8 @@
+try {
+    Push-Location $PSScriptRoot
+    dotnet run --project "./tools/FakeItEasy.Build" -- $args
+    if ($LASTEXITCODE) { Throw "Build failed with exit code $LASTEXITCODE." }
+}
+finally {
+    Pop-Location
+}
