b436bf1a931b24e965f38b1f9f1f20e8406a16b8
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2022-11-27 20:29:55 -0500

    Add release workflow

--- tools/deploy.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-git submodule -q update --init
-if %errorlevel% neq 0 exit /b %errorlevel%
-dotnet run --project "%~dp0..\tools-shared\FakeItEasy.Deploy\FakeItEasy.Deploy.csproj" -- "%~dp0..\artifacts\output" %*
