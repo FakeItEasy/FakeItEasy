6bd612cb63428223dadbf4498f91de97fd4b8602
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2023-07-12 00:50:04 -0400

    Add generic base class for custom argument equality comparers

--- src/FakeItEasy/Core/ArgumentEqualityComparer.cs
+++ src/FakeItEasy/Core/ArgumentEqualityComparer.cs
@@ -1,24 +1,23 @@
 ﻿namespace FakeItEasy.Core
 {
     using System;
+    using System.Collections.Concurrent;
     using System.Collections.Generic;
     using System.Linq;
 
     internal class ArgumentEqualityComparer
     {
         private readonly IEnumerable<IArgumentEqualityComparer> argumentEqualityComparers;
+        private readonly ConcurrentDictionary<Type, IArgumentEqualityComparer?> cachedComparers = new();
 
         public ArgumentEqualityComparer(IEnumerable<IArgumentEqualityComparer> argumentEqualityComparers)
         {
-            this.argumentEqualityComparers = argumentEqualityComparers;
+            this.argumentEqualityComparers = argumentEqualityComparers.OrderByDescending(c => c.Priority).ToArray();
         }
 
         public bool AreEqual(object? expectedValue, object? argumentValue, Type parameterType)
         {
-            var comparer = this.argumentEqualityComparers
-                .Where(c => c.CanCompare(parameterType))
-                .OrderByDescending(c => c.Priority)
-                .FirstOrDefault();
+            var comparer = this.cachedComparers.GetOrAdd(parameterType, t => this.argumentEqualityComparers.FirstOrDefault(c => c.CanCompare(t)));
 
             if (comparer is null)
             {
