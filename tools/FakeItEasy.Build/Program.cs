631f1532811dab0fa53b3f0b90729911f8933662
Blair Conrad <blair@blairconrad.com>
2024-10-03 15:22:57 -0400

    Generate .python-version file from pyproject.toml

--- tools/FakeItEasy.Build/Program.cs
+++ tools/FakeItEasy.Build/Program.cs
@@ -1,4 +1,6 @@
+using System.Collections.Frozen;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using SimpleExec;
 using static Bullseye.Targets;
@@ -69,7 +71,27 @@ Target(
 
 Target(
     "generate-docs",
+    DependsOn("create-python-version-file"),
     () => Run("uv", "run mkdocs build --clean --site-dir artifacts/docs --config-file mkdocs.yml --strict"));
+
+// uv really likes there to be a .python-version file to specify which version of python it should use.
+// Rather than maintain a duplicate of the version in pyproject.toml, we'll generate the .python-version file from the latter.
+Target(
+    "create-python-version-file",
+    () =>
+    {
+        // expect the line to look something like
+        // requires-python = "~=x.yz"
+        // . We want the x.yz part.
+        const string versionSourceFile = "pyproject.toml";
+        const string versionPattern = @"requires-python = ""~=(?<version>[^""]+)""";
+        var pythonVersion = File.ReadLines(versionSourceFile)
+            .Select(line => Regex.Match(line, versionPattern))
+            .Where(m => m.Success)
+            .Select(m => m.Groups["version"].Value)
+            .FirstOrDefault() ?? throw new InvalidOperationException($"Could not find required Python version in {versionSourceFile}");
+        File.WriteAllText(".python-version", pythonVersion);
+    });
 
 Target(
     "force-approve",
