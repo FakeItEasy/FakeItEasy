3aa9d18e758fd14825d1d83247791d91599d9d49
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2026-01-02 17:24:58 +0100

    Add HasSameElementsAs argument constraint

--- src/FakeItEasy/ArgumentConstraintManagerExtensions.cs
+++ src/FakeItEasy/ArgumentConstraintManagerExtensions.cs
@@ -253,6 +253,51 @@ namespace FakeItEasy
         }
 
         /// <summary>
+        /// Constrains the argument collection so that it must contain the same elements as the
+        /// specified collection, in any order.
+        /// </summary>
+        /// <param name="manager">The constraint manager to match the constraint.</param>
+        /// <param name="values">The sequence to test against.</param>
+        /// <param name="comparer">A comparer to test the collection elements for equality. If null, the default equality comparer for <see cref="TElement"/> will be used.</param>
+        /// <typeparam name="T">The type of argument to constrain.</typeparam>
+        /// <typeparam name="TElement">The type of the collection's elements.</typeparam>
+        /// <returns>A dummy argument value.</returns>
+        public static T HasSameElementsAs<T, TElement>(
+            this IArgumentConstraintManager<T> manager,
+            IEnumerable<TElement> values,
+            IEqualityComparer<TElement>? comparer = null)
+            where T : IEnumerable<TElement>
+        {
+            Guard.AgainstNull(manager);
+            Guard.AgainstNull(values);
+
+            var list = values.AsList();
+            return manager.NullCheckedMatches(
+                x => x.HasSameElementsAs(list, comparer),
+                x => x.WriteArgumentValues(list).Write(" (in any order)"));
+        }
+
+        /// <summary>
+        /// Constrains the argument collection so that it must contain the same elements as the
+        /// specified collection, in any order.
+        /// </summary>
+        /// <param name="manager">The constraint manager to match the constraint.</param>
+        /// <param name="values">The sequence to test against.</param>
+        /// <typeparam name="T">The type of argument to constrain.</typeparam>
+        /// <typeparam name="TElement">The type of the collection's elements.</typeparam>
+        /// <returns>A dummy argument value.</returns>
+        public static T HasSameElementsAs<T, TElement>(
+            this IArgumentConstraintManager<T> manager,
+            params TElement[] values)
+            where T : IEnumerable<TElement>
+        {
+            Guard.AgainstNull(manager);
+            Guard.AgainstNull(values);
+
+            return manager.HasSameElementsAs((IEnumerable<TElement>)values);
+        }
+
+        /// <summary>
         /// Tests that the IEnumerable contains no items.
         /// </summary>
         /// <typeparam name="T">The type of argument.</typeparam>
--- src/FakeItEasy/EnumerableExtensions.cs
+++ src/FakeItEasy/EnumerableExtensions.cs
@@ -61,5 +61,111 @@ namespace FakeItEasy
         {
             return source as IList<object> ?? source.Cast<object>().ToList();
         }
+
+        public static bool HasSameElementsAs<T>(
+            this IEnumerable<T> collection,
+            IEnumerable<T> other,
+            IEqualityComparer<T>? comparer)
+        {
+            Guard.AgainstNull(collection);
+            Guard.AgainstNull(other);
+
+            // Fail fast if counts differ, when we can get the count without enumerating
+            if (collection.TryGetCount(out int collectionCount)
+                && other.TryGetCount(out int otherCount)
+                && collectionCount != otherCount)
+            {
+                return false;
+            }
+
+            var boxComparer = new BoxEqualityComparer<T>(comparer);
+            var elementCounts = other
+                .GroupBy(x => new Box<T>(x), boxComparer)
+                .ToDictionary(g => g.Key, g => g.Count(), boxComparer);
+            foreach (var element in collection)
+            {
+                var box = new Box<T>(element);
+                if (!elementCounts.TryGetValue(box, out var count))
+                {
+                    // collection contains element that isn't in other
+                    return false;
+                }
+
+                if (--count == 0)
+                {
+                    elementCounts.Remove(box);
+                }
+                else
+                {
+                    elementCounts[box] = count;
+                }
+            }
+
+            // Ensure we consumed all elements from other.
+            // If not, collection had fewer elements than other
+            return elementCounts.Count == 0;
+        }
+
+        /// <summary>
+        /// Tries to get the count of items in the collection without enumerating it.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="count">The count of items, if it could be determined without enumeration; otherwise, zero.</param>
+        /// <typeparam name="T">The type of items in the collection.</typeparam>
+        /// <returns>true if the count could be determined without enumeration; otherwise, false.</returns>
+        public static bool TryGetCount<T>(this IEnumerable<T> collection, out int count)
+        {
+#if LACKS_NONENUMERATEDCOUNT
+            if (collection is ICollection<T> genericCollection)
+            {
+                count = genericCollection.Count;
+                return true;
+            }
+
+            if (collection is ICollection nonGenericCollection)
+            {
+                count = nonGenericCollection.Count;
+                return true;
+            }
+
+            if (collection is IReadOnlyCollection<T> readOnlyCollection)
+            {
+                count = readOnlyCollection.Count;
+                return true;
+            }
+
+            count = 0;
+            return false;
+#else
+            return collection.TryGetNonEnumeratedCount(out count);
+#endif
+        }
+
+        /// <summary>
+        /// Wraps a possibly null value in a struct to allow usage as a key in dictionaries.
+        /// </summary>
+        /// <param name="Value">The value to wrap.</param>
+        /// <typeparam name="T">The type of the value.</typeparam>
+        private record struct Box<T>(T Value);
+
+        private class BoxEqualityComparer<T> : IEqualityComparer<Box<T>>
+        {
+            private readonly IEqualityComparer<T> comparer;
+
+            public BoxEqualityComparer(IEqualityComparer<T>? comparer)
+            {
+                this.comparer = comparer ?? EqualityComparer<T>.Default;
+            }
+
+            public bool Equals(Box<T> x, Box<T> y)
+            {
+                return this.comparer.Equals(x.Value, y.Value);
+            }
+
+            public int GetHashCode(Box<T> obj)
+            {
+                return this.comparer.GetHashCode(obj.Value!);
+            }
+        }
     }
 }
